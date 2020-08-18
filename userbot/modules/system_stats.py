f98945de6790ca33f72d890c8de8c0ec89440268
X Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-08-18 19:06:08 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -221,6 +221,7 @@ async def pipcheck(pip):
 @register(outgoing=True, pattern=r"^\.(?:alive|on)\s?(.)?")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
+    uptime = await get_readable_time((time.time() - StartTime))
     logo = ALIVE_LOGO
     output = (f"۝⩵꙰ཱི►XBOT-REMIX◄⩵꙰ཱི۝\n is running on [ `{UPSTREAM_REPO_BRANCH}` ]\n"
              f"╭━━━━━━━━━━━━━━━━━━━━━╮\n"
