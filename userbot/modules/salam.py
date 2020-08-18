d56ac270ad4475eed8524eb013543a78eefad97e
X Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-08-18 15:32:16 +0700

    Add files via upload

---
+++ userbot/modules/salam.py
@@ -0,0 +1,56 @@
+import asyncio
+import re
+import time
+from time import sleep
+from platform import python_version, uname
+from userbot import CMD_HELP, ZALG_LIST, ALIVE_NAME
+from userbot.events import register
+
+# ================= CONSTANT =================
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+# ============================================
+
+@register(outgoing=True, pattern='^P(?: |$)(.*)')
+async def typewriter(typew):
+	message = typew.pattern_match.group(1)
+	sleep(1)
+	await typew.edit("Hallo Semua Saya `{DEFAULTUSER}`")
+	sleep(2)
+	await typew.edit("`Assalamualaikum.....😚`")
+# Owner @Si_Dian
+
+@register(outgoing=True, pattern='^p(?: |$)(.*)')
+async def typewriter(typew):
+	message = typew.pattern_match.group(1)
+	sleep(1)
+	await typew.edit("Hallo Semua Saya `{DEFAULTUSER}`")
+	sleep(2)
+	await typew.edit("`Assalamualaikum.....😚`")
+# Owner @Si_Dian
+
+@register(outgoing=True, pattern='^L(?: |$)(.*)')
+async def typewriter(typew):
+	message = typew.pattern_match.group(1)
+	sleep(1)
+	await typew.edit("`Astaghfirulloh Jawab Salam Dong...`")
+	sleep(1)
+	await typew.edit("`Waallaikumsalam......`")
+# Owner @Si_Dian
+
+@register(outgoing=True, pattern='^l(?: |$)(.*)')
+async def typewriter(typew):
+	message = typew.pattern_match.group(1)
+	sleep(1)
+	await typew.edit("`Astaghfirulloh Jawab Salam Dong...`")
+	sleep(1)
+	await typew.edit("`Waallaikumsalam.....`")
+# Owner @Si_Dian
+
+
+CMD_HELP.update({
+    "salam":
+    "`P`\
+\nUsage: Untuk Memberi salam.\
+\n\n`L`\
+\nUsage: Untuk Menjawab Salam."   
+})  
