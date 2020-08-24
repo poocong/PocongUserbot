815cba857980d8e6e5a84c4c7fa58cbba188de04
X Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-08-24 12:05:08 +0700

    Update scrapers.py

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -187,7 +187,7 @@ async def img_sampler(event):
         lim = lim.replace("lim=", "")
         query = query.replace("lim=" + lim[0], "")
     except IndexError:
-        lim = 6
+        lim = 4
     response = googleimagesdownload()
 
     # creating list of arguments
