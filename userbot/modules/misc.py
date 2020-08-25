ffc9ef5b05da02efe5264ba241d477b1261f247d
X Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-08-25 18:19:01 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -118,7 +118,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "╭━━━━━━━━𖠄━━━━━━━━╮\n[┣━─━─[ XBOT-REMIX ]─━─━┫](https://github.com/X-Newbie/XBot-Remix) \n╰━━━━━━━━𖠄━━━━━━━━╯\n     \n"
+        "╭━━━━━━━━𖠄━━━━━━━━╮\n [┣━─━─[ XBOT-REMIX ]─━─━┫](https://github.com/X-Newbie/XBot-Remix) \n ╰━━━━━━━━𖠄━━━━━━━━╯\n     \n"
     )
 
 
