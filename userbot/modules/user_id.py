63899829fcecc11f579730422218c9ec1054d037
X Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-08-26 11:23:25 +0700

    Update user_id.py

--- userbot/modules/user_id.py
+++ userbot/modules/user_id.py
@@ -7,39 +7,39 @@ from userbot.events import register
 
 
 @register(outgoing=True, pattern=r"^\.gid(?: |$)(.*)")
-async def (event):
-    if event.fwd_from:
+async def firstname(uid):
+    if uid.fwd_from:
         return 
-    if not event.reply_to_msg_id:
-       await event.edit("```Balas di Pesan Goblok!!.```")
+    if not uid.reply_to_msg_id:
+       await uid.edit("```Balas di Pesan Goblok!!.```")
        return
-    message = await event.get_reply_message() 
+    message = await uid.get_reply_message() 
     chat = "@getidsbot"
     user_id = message.sender.id
     id = f"/search_id {user_id}"
     sender = reply_message.sender
     if reply_message.sender.bot:
-       await event.edit("```Balas di Pesan Goblok!!.```")
+       await uid.edit("```Balas di Pesan Goblok!!.```")
        return
-    await event.edit("```Membongkar ID..```")
+    await uid.edit("```Membongkar ID..```")
     async with borg.conversation(chat) as conv:
           try:
                 msg = await conv.send_message(id)
                 r = await conv.get_response()
                 response = await conv.get_response()
             except YouBlockedUserError:
-                await event.reply("`Bunuh @getidsbot dulu bos, biar botnya bisa jalan -_-`")
+                await uid.reply("`Bunuh @getidsbot dulu bos, biar botnya bisa jalan -_-`")
                 return
           if response.text.startswith("Profil Buriq Tidak Punya ID,"):
-             await event.edit("```Profil Buriq Tidak Punya ID..```")
-          await event.client.delete_messages(
+             await uid.edit("```Profil Buriq Tidak Punya ID..```")
+          await uid.client.delete_messages(
                     conv.chat_id, [msg.id, r.id, response.id]
                 )
                 return
             else:
                 respond = await conv.get_response()
-                await event.edit(f"{response.message}")
-            await event.client.delete_messages(
+                await uid.edit(f"{response.message}")
+            await uid.client.delete_messages(
                 conv.chat_id, [msg.id, r.id, response.id, respond.id]
             )
     except TimeoutError:
