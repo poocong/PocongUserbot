f80aedd3a67c23aa1ce93cfe0f9c8b3cd95b2b3a
X Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-08-26 20:08:23 +0700

    Add files via upload

--- userbot/modules/memify.py
+++ userbot/modules/memify.py
@@ -1,4 +1,146 @@
-#Encript Marshal By XVenom
-#https://github.com/xvenom15
-import marshal
-exec(marshal.loads(b'\xe3\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x04\x00\x00\x00@\x00\x00\x00sr\x01\x00\x00d\x00d\x01l\x00m\x01Z\x01\x01\x00d\x00d\x02l\x02m\x03Z\x03\x01\x00d\x00d\x03l\x04m\x05Z\x05\x01\x00d\x00d\x04l\x06m\x07Z\x07\x01\x00d\x00d\x05l\x08Z\x08d\x00d\x05l\tZ\td\x00d\x06l\nm\nZ\n\x01\x00d\x00d\x07l\x0bm\x0cZ\x0c\x01\x00d\x00d\x08l\rm\x0eZ\x0e\x01\x00d\x00d\tl\x0fm\x10Z\x10\x01\x00d\x00d\nl\x11m\x12Z\x12\x01\x00d\x00d\x0bl\x13m\x14Z\x14m\x15Z\x15m\x16Z\x16\x01\x00d\x00d\x0cl\x17m\x18Z\x18m\x19Z\x19m\x1aZ\x1a\x01\x00d\x00d\rl\x1bm\x1cZ\x1c\x01\x00d\x00d\x05l\nZ\nd\x00d\x0el\x1dm\x1eZ\x1e\x01\x00d\x00d\x05l\x1fZ\x1fd\x00d\x05l Z d\x00d\x05l!Z!d\x00d\x05l"Z"d\x00d\x0fl\x00m#Z#\x01\x00d\x00d\x10l$m%Z%\x01\x00d\x00d\x11l&m\'Z\'\x01\x00d\x00d\x12l(m)Z)\x01\x00d\x00d\x13l\x11m*Z*m+Z+m,Z,m-Z-m.Z.m/Z/m0Z0\x01\x00d\x14Z1e\x1cd\x15d\x16d\x17\x8d\x02d\x18d\x19\x84\x00\x83\x01Z2d\x1ad\x1b\x84\x00Z3d\x1cd\x1d\x84\x00Z4e\x19\xa05d\x1ed\x1fi\x01\xa1\x01\x01\x00d\x05S\x00) \xe9\x00\x00\x00\x00)\x01\xda\x13YouBlockedUserError)\x01\xda\x06events)\x01\xda\x07BytesIO)\x01\xda\x05ImageN)\x01\xda\x08datetime)\x01\xda\x0fextractMetadata)\x01\xda\x0ccreateParser)\x01\xda\x07SmartDL)\x01\xda\x16DocumentAttributeVideo)\x03\xda\x08progress\xda\nhumanbytes\xda\x0etime_formatter)\x03\xda\x17TEMP_DOWNLOAD_DIRECTORY\xda\x08CMD_HELP\xda\x03bot)\x01\xda\x08register)\x01\xda\x0bdefaultdict)\x01\xda\x16StickersetInvalidError)\x01\xda\x17MessageNotModifiedError)\x01\xda\x1bUpdateNotifySettingsRequest)\x01\xda\x14GetStickerSetRequest)\x07\xda\x19DocumentAttributeFilename\xda\x18DocumentAttributeSticker\xda\x1aInputMediaUploadedDocument\xda\x17InputPeerNotifySettings\xda\x11InputStickerSetID\xda\x18InputStickerSetShortName\xda\x11MessageMediaPhotoz\x11./thumb_image.jpgTz\x10^.mmf(?: |$)(.*))\x02Z\x08outgoingZ\x07patternc\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x13\x00\x00\x00\n\x00\x00\x00\xc3\x00\x00\x00s\x1e\x03\x00\x00|\x00j\x00r\nd\x00S\x00|\x00j\x01s$|\x00\xa0\x02d\x01\xa1\x01I\x00d\x00H\x00\x01\x00d\x00S\x00|\x00\xa0\x03\xa1\x00I\x00d\x00H\x00}\x01|\x01j\x04sL|\x00\xa0\x02d\x02\xa1\x01I\x00d\x00H\x00\x01\x00d\x00S\x00d\x03}\x02|\x01j\x05}\x03d\x04}\x04t\x06\xa0\x07|\x01j\x04\xa1\x01I\x00d\x00H\x00}\x05d\x00}\x06|\x01j\x05j\x06r\x8c|\x00\xa0\x02d\x05\xa1\x01I\x00d\x00H\x00\x01\x00d\x00S\x00|\x00\xa0\x02d\x06\xa1\x01I\x00d\x00H\x00\x01\x00t\x08\xa0\td\x07\xa1\x01I\x00d\x00H\x00\x01\x00t\x06\xa0\nd\x03\xa1\x014\x00I\x00d\x00H\x00\x90\x02\x9aN}\x07zb|\x00j\x0b\xa0\x0cd\x08\xa1\x01}\x08t\r|\x07d\t\x83\x02I\x00d\x00H\x00\x01\x00t\x08\xa0\td\x08\xa1\x01I\x00d\x00H\x00\x01\x00t\r|\x07|\x08\x83\x02I\x00d\x00H\x00\x01\x00t\x06\xa0\x0e|\x02|\x01j\x04\xa1\x02I\x00d\x00H\x00\x01\x00|\x07\xa0\x0f\xa1\x00I\x00d\x00H\x00}\tW\x00n8\x04\x00t\x10k\n\x90\x01r\\\x01\x00\x01\x00\x01\x00|\x00\xa0\x11d\n\xa1\x01I\x00d\x00H\x00\x01\x00Y\x00W\x005\x00Q\x00I\x00d\x00H\x00R\x00\xa3\x00d\x00S\x00X\x00|\tj\x12\xa0\x13d\x0b\xa1\x01\x90\x01r||\x00\xa0\x02d\x0c\xa1\x01I\x00d\x00H\x00\x01\x00d\r|\tj\x12k\x06\x90\x02r\xc6|\x00\xa0\x02d\x0e\xa1\x01I\x00d\x00H\x00\x01\x00d\x00}\nt\x14j\x15\xa0\x16t\x17\xa1\x01\x90\x01r\xaet\x17}\n|\x00j\x0b\xa0\x0cd\x08\xa1\x01}\x0bt\x14j\x15\xa0\x18t\x19\xa1\x01\x90\x01s\xd2t\x14\xa0\x1at\x19\xa1\x01\x01\x00|\x00j\x01\x90\x02r\\d\x0f}\x0c|\x00\xa0\x03\xa1\x00I\x00d\x00H\x00}\x01t\x19}\rt\x14j\x15\xa0\x1b|\r|\x0c\xa1\x02}\x0et\x06\xa0\x1c|\x01|\x0e\xa1\x02I\x00d\x00H\x00}\x0et\x14j\x15\xa0\x16|\x0e\xa1\x01\x90\x02rFt\x06j\x0e|\x02|\x0ed\x10d\x10d\x10|\nd\x11\x8d\x06I\x00d\x00H\x00\x01\x00t\x14\xa0\x1d|\x0e\xa1\x01\x01\x00n\x16|\x00\xa0\x02d\x12\xa0\x1e|\x0b\xa1\x01\xa1\x01I\x00d\x00H\x00\x01\x00|\x07\xa0\x0f\xa1\x00I\x00d\x00H\x00}\tt\x19}\x0fd\x13}\x10t\x14j\x15\xa0\x1b|\x0f|\x10\xa1\x02}\x11t\x06\xa0\x1c|\tj\x04|\x11\xa1\x02I\x00d\x00H\x00\x01\x00t\x19d\x13\x17\x00}\x12t\x06j\x0e|\x00j\x1f|\x12d\x10d\x14d\x15\x8d\x04I\x00d\x00H\x00\x01\x00|\x00\xa0 \xa1\x00I\x00d\x00H\x00\x01\x00nDt!|\x01\x83\x01\x90\x02s\xf4|\x00\xa0\x02d\x16\xa1\x01I\x00d\x00H\x00\x01\x00W\x005\x00Q\x00I\x00d\x00H\x00R\x00\xa3\x00d\x00S\x00t\x06\xa0\x0e|\x00j\x1f|\tj\x04\xa1\x02I\x00d\x00H\x00\x01\x00W\x005\x00Q\x00I\x00d\x00H\x00R\x00X\x00d\x00S\x00)\x17NzG`Syntax: reply to an image with .mmf` \'text on top\' ; \'text on bottom\' z"```reply to a image/sticker/gif```z\x0c@MemeAutobotz\r@memetime.pngz$```Reply to actual users message.```uL\x00\x00\x00```Transfiguration Time! Mwahaha Memifying this image! (\xe3\x80\x8d\xef\xbe\x9f\xef\xbe\x9b\xef\xbe\x9f)\xef\xbd\xa3 ```\xe9\x05\x00\x00\x00\xe9\x01\x00\x00\x00z\x06/startz/```Please unblock @MemeAutobot and try again```Z\x07ForwardzK```can you kindly disable your forward privacy settings for good, Nibba?```z\x07Okay...uh\x00\x00\x00```\xf0\x9f\x9b\x91 \xf0\x9f\xa4\xa8 NANI?! This is not an image! This will take sum tym to convert to image... UwU \xf0\x9f\xa7\x90 \xf0\x9f\x9b\x91```z\x08meme.pngF)\x04Z\x0eforce_document\xda\x12supports_streamingZ\x0ballow_cache\xda\x05thumbz\x11File Not Found {}z\nmemes.webpZ\x08Memifyed)\x02r \x00\x00\x00Z\x07captionz<Invalid message type. Plz choose right message type u NIBBA.)"Z\x08fwd_fromZ\x0freply_to_msg_idZ\x04editZ\x11get_reply_message\xda\x05media\xda\x06senderr\x10\x00\x00\x00Z\rdownload_file\xda\x07asyncio\xda\x05sleepZ\x0cconversationZ\rpattern_match\xda\x05group\xda\x15silently_send_messageZ\tsend_file\xda\x0cget_responser\x02\x00\x00\x00Z\x05reply\xda\x04text\xda\nstartswith\xda\x02os\xda\x04path\xda\x06exists\xda\x10THUMB_IMAGE_PATH\xda\x05isdirr\x0e\x00\x00\x00\xda\x08makedirs\xda\x04joinZ\x0edownload_media\xda\x06remove\xda\x06formatZ\x07chat_id\xda\x06delete\xda\x10is_message_image)\x13Z\x05eventZ\rreply_messageZ\x04chatr#\x00\x00\x00Z\x0ffile_ext_ns_ion\xda\x04fileZ\x0cuploaded_gifZ\x08bot_convZ\x07memeVar\xda\x08responser!\x00\x00\x00Z\tinput_str\xda\tfile_nameZ\x15to_download_directoryZ\x14downloaded_file_nameZ\x16the_download_directoryZ\nfiles_nameZ\x12download_file_nameZ\x12requires_file_name\xa9\x00r9\x00\x00\x00\xda\x00\xda\x03mim*\x00\x00\x00s\xa0\x00\x00\x00\x00\x02\x06\x01\x04\x01\x06\x01\x10\x01\x04\x01\x0e\x01\x06\x01\x10\x01\x04\x01\x04\x01\x06\x01\x04\x01\x12\x01\x04\x01\x08\x01\x10\x01\x04\x02\x10\x01\x10\x02\x16\x01\x02\x01\x0c\x01\x10\x01\x10\x01\x10\x01\x14\x01\x12\x01\x10\x01\x10\x01\x18\x01\x0e\x01\x10\x01\x0c\x01\x10\x01\x04\x01\x0e\x01\x04\x01\x0c\x01\x0e\x01\n\x01\x08\x01\x04\x01\x0e\x01\x04\x01\x0e\x01\x04\x01\x02\x01\x02\xfe\n\x04\x0e\x01\x04\x01\x02\x01\x02\x01\x02\x01\x02\x01\x02\x01\x02\xfa\x0c\x08\x0c\x02\x16\x01\x0e\x01\x04\x01\x04\x01\x0e\x01\x04\x01\x04\x01\x02\xfe\n\x04\x08\x01\x04\x01\x04\x01\x02\x01\x02\x01\x02\xfc\x0c\x06\x10\x02\n\x01\x10\x01\x14\x02r;\x00\x00\x00c\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x03\x00\x00\x00C\x00\x00\x00sB\x00\x00\x00|\x00j\x00r>t\x01|\x00j\x00t\x02\x83\x02r\x16d\x01S\x00|\x00j\x00j\x03r:|\x00j\x00j\x03j\x04\xa0\x05d\x02\xa1\x01d\x03\x19\x00d\x04k\x02r:d\x01S\x00d\x05S\x00d\x05S\x00)\x06NT\xfa\x01/r\x01\x00\x00\x00Z\x05imageF)\x06r"\x00\x00\x00\xda\nisinstancer\x1d\x00\x00\x00Z\x08documentZ\tmime_type\xda\x05split\xa9\x01\xda\x07messager9\x00\x00\x00r9\x00\x00\x00r:\x00\x00\x00r5\x00\x00\x00\x82\x00\x00\x00s\x10\x00\x00\x00\x00\x01\x06\x01\x0c\x01\x04\x01\x08\x01\x18\x01\x04\x01\x04\x01r5\x00\x00\x00c\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x03\x00\x00\x00\x03\x00\x00\x00\xc3\x00\x00\x00s4\x00\x00\x00|\x00\xa0\x00|\x01\xa1\x01I\x00d\x00H\x00\x01\x00|\x00\xa0\x01\xa1\x00I\x00d\x00H\x00}\x02|\x00j\x02|\x02d\x01\x8d\x01I\x00d\x00H\x00\x01\x00|\x02S\x00)\x02Nr?\x00\x00\x00)\x03Z\x0csend_messager(\x00\x00\x00Z\tmark_read)\x03Z\x04convr)\x00\x00\x00r7\x00\x00\x00r9\x00\x00\x00r9\x00\x00\x00r:\x00\x00\x00r\'\x00\x00\x00\x8c\x00\x00\x00s\x08\x00\x00\x00\x00\x01\x10\x01\x0e\x01\x12\x01r\'\x00\x00\x00Z\x06memifyzW`.mmf` texttop ; textbottom        \nUsage: Reply a sticker/image/gif and send with cmd.)6Z\x1ctelethon.errors.rpcerrorlistr\x02\x00\x00\x00Z\x08telethonr\x03\x00\x00\x00\xda\x02ior\x04\x00\x00\x00Z\x03PILr\x05\x00\x00\x00r$\x00\x00\x00\xda\x04timer\x06\x00\x00\x00Z\x10hachoir.metadatar\x07\x00\x00\x00Z\x0ehachoir.parserr\x08\x00\x00\x00Z\tpySmartDLr\t\x00\x00\x00Z\x11telethon.tl.typesr\n\x00\x00\x00Z\ruserbot.utilsr\x0b\x00\x00\x00r\x0c\x00\x00\x00r\r\x00\x00\x00Z\x07userbotr\x0e\x00\x00\x00r\x0f\x00\x00\x00r\x10\x00\x00\x00Z\x0euserbot.eventsr\x11\x00\x00\x00Z\x0bcollectionsr\x12\x00\x00\x00Z\x04mathr+\x00\x00\x00Z\x08requestsZ\x07zipfiler\x13\x00\x00\x00Z\x0ftelethon.errorsr\x14\x00\x00\x00Z\x1dtelethon.tl.functions.accountr\x15\x00\x00\x00Z\x1etelethon.tl.functions.messagesr\x16\x00\x00\x00r\x17\x00\x00\x00r\x18\x00\x00\x00r\x19\x00\x00\x00r\x1a\x00\x00\x00r\x1b\x00\x00\x00r\x1c\x00\x00\x00r\x1d\x00\x00\x00r.\x00\x00\x00r;\x00\x00\x00r5\x00\x00\x00r\'\x00\x00\x00\xda\x06updater9\x00\x00\x00r9\x00\x00\x00r9\x00\x00\x00r:\x00\x00\x00\xda\x08<module>\t\x00\x00\x00sB\x00\x00\x00\x0c\x01\x0c\x01\x0c\x01\x0c\x01\x08\x01\x08\x01\x0c\x01\x0c\x01\x0c\x01\x0c\x01\x0c\x01\x14\x01\x14\x01\x0c\x01\x08\x01\x0c\x01\x08\x01\x08\x01\x08\x01\x08\x01\x0c\x01\x0c\x01\x0c\x01\x0c\x01$\x06\x04\x03\n\x01\nW\x08\n\x08\x06\x04\x01\x02\x01\x02\xfe'))
+import os
+import textwrap
+
+from PIL import Image, ImageFont, ImageDraw
+
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon import events
+from io import BytesIO
+from PIL import Image
+import asyncio
+import time
+from datetime import datetime
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+from pySmartDL import SmartDL
+from telethon.tl.types import DocumentAttributeVideo
+from userbot.utils import progress, humanbytes, time_formatter, take_screen_shot
+from userbot import (Message, TEMP_DOWNLOAD_DIRECTORY, CMD_HELP, bot)
+from userbot.events import register
+import datetime
+from collections import defaultdict
+import math
+import os
+import requests
+import zipfile
+from telethon.errors.rpcerrorlist import StickersetInvalidError
+from telethon.errors import MessageNotModifiedError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+from telethon.tl.functions.messages import GetStickerSetRequest
+from telethon.tl.types import (DocumentAttributeFilename, DocumentAttributeSticker,
+                               InputMediaUploadedDocument, InputPeerNotifySettings,
+                               InputStickerSetID, InputStickerSetShortName,
+                               MessageMediaPhoto)
+
+
+@register(outgoing=True, pattern="^.mmf(?: |$)(.*)")
+async def memify(message: Message):
+    replied = message.reply_to_message
+    if not replied:
+        await message.err("LMAO no one's gonna help you, if u use .help now then u **Gey**")
+        await message.client.send_sticker(
+            sticker="CAADAQADhAAD3gkwRviGxMVn5813FgQ", chat_id=message.chat.id)
+        return
+    if not (replied.photo or replied.sticker or replied.animation):
+        await message.err("Bruh, U Comedy me? Read help or gtfo (¬_¬)")
+        return
+    if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
+    await message.edit("He he, let me use my skills")
+    dls = await message.client.download_media(
+        message=message.reply_to_message,
+        file_name=TEMP_DOWNLOAD_DIRECTORY,
+        progress=progress,
+        progress_args=(message, "Trying to Posses given content")
+    )
+    dls_loc = os.path.join(TEMP_DOWNLOAD_DIRECTORY, os.path.basename(dls))
+    if replied.sticker and replied.sticker.file_name.endswith(".tgs"):
+        await message.edit("OMG, an Animated sticker ⊙_⊙, lemme do my bleck megik...")
+        png_file = os.path.join(TEMP_DOWNLOAD_DIRECTORY, "meme.png")
+        cmd = f"lottie_convert.py --frame 0 -if lottie -of png {dls_loc} {png_file}"
+        stdout, stderr = (await runcmd(cmd))[:2]
+        os.remove(dls_loc)
+        if not os.path.lexists(png_file):
+            await message.err("This sticker is Gey, i won't memify it ≧ω≦")
+            raise Exception(stdout + stderr)
+        dls_loc = png_file
+    elif replied.animation:
+        await message.edit("Look it's GF. Oh, no it's just a Gif ")
+        jpg_file = os.path.join(TEMP_DOWNLOAD_DIRECTORY, "meme.jpg")
+        await take_screen_shot(dls_loc, 0, jpg_file)
+        os.remove(dls_loc)
+        if not os.path.lexists(jpg_file):
+            await message.err("This Gif is Gey (｡ì _ í｡), won't memify it.")
+            return
+        dls_loc = jpg_file
+    await message.edit("Decoration Time ≧∇≦, I'm an Artist")
+    webp_file = await draw_meme_text(dls_loc, message.input_str)
+    await message.client.send_sticker(chat_id=message.chat.id,
+                                      sticker=webp_file,
+                                      reply_to_message_id=replied.message_id)
+    await message.delete()
+    os.remove(webp_file)
+
+
+async def draw_meme_text(image_path, text):
+    img = Image.open(image_path)
+    os.remove(image_path)
+    i_width, i_height = img.size
+    m_font = ImageFont.truetype("userbot/utils/fontx.ttf", int((70 / 640) * i_width))
+    if ";" in text:
+        upper_text, lower_text = text.split(";")
+    else:
+        upper_text = text
+        lower_text = ''
+    draw = ImageDraw.Draw(img)
+    current_h, pad = 10, 5
+    if upper_text:
+        for u_text in textwrap.wrap(upper_text, width=15):
+            u_width, u_height = draw.textsize(u_text, font=m_font)
+
+            draw.text(xy=(((i_width - u_width) / 2) - 1, int((current_h / 640)*i_width)),
+                      text=u_text, font=m_font, fill=(0, 0, 0))
+            draw.text(xy=(((i_width - u_width) / 2) + 1, int((current_h / 640)*i_width)),
+                      text=u_text, font=m_font, fill=(0, 0, 0))
+            draw.text(xy=((i_width - u_width) / 2, int(((current_h / 640)*i_width)) - 1),
+                      text=u_text, font=m_font, fill=(0, 0, 0))
+            draw.text(xy=(((i_width - u_width) / 2), int(((current_h / 640)*i_width)) + 1),
+                      text=u_text, font=m_font, fill=(0, 0, 0))
+
+            draw.text(xy=((i_width - u_width) / 2, int((current_h / 640)*i_width)),
+                      text=u_text, font=m_font, fill=(255, 255, 255))
+            current_h += u_height + pad
+    if lower_text:
+        for l_text in textwrap.wrap(lower_text, width=15):
+            u_width, u_height = draw.textsize(l_text, font=m_font)
+
+            draw.text(
+                xy=(((i_width - u_width) / 2) - 1, i_height - u_height - int((20 / 640)*i_width)),
+                text=l_text, font=m_font, fill=(0, 0, 0))
+            draw.text(
+                xy=(((i_width - u_width) / 2) + 1, i_height - u_height - int((20 / 640)*i_width)),
+                text=l_text, font=m_font, fill=(0, 0, 0))
+            draw.text(
+                xy=((i_width - u_width) / 2, (i_height - u_height - int((20 / 640)*i_width)) - 1),
+                text=l_text, font=m_font, fill=(0, 0, 0))
+            draw.text(
+                xy=((i_width - u_width) / 2, (i_height - u_height - int((20 / 640)*i_width)) + 1),
+                text=l_text, font=m_font, fill=(0, 0, 0))
+
+            draw.text(
+                xy=((i_width - u_width) / 2, i_height - u_height - int((20 / 640)*i_width)),
+                text=l_text, font=m_font, fill=(255, 255, 255))
+            current_h += u_height + pad
+
+    image_name = "memify.webp"
+    webp_file = os.path.join(TEMP_DOWNLOAD_DIRECTORY, image_name)
+    img.save(webp_file, "WebP")
+    return webp_file
+
+
+
+CMD_HELP.update({
+    "memify": 
+        "`.mmf` texttop ; textbottom\
+        \nUsage: Reply a sticker/image/gif and send with cmd."
+})
