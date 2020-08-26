dee24da121a57005ffc851f44b2aeae84c403e9f
X Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-08-26 20:15:25 +0700

    Update tools.py

--- userbot/utils/tools.py
+++ userbot/utils/tools.py
@@ -77,15 +77,3 @@ async def is_admin(chat_id, user_id):
     if isinstance(chat_participant, ChannelParticipantCreator) or isinstance(chat_participant, ChannelParticipantAdmin):
         return True
     return False
-
-
-async def take_screen_shot(video_file: str, duration: int, path: str) -> str:
-    """ take a screenshot """
-    _LOG.info('[[[Extracting a frame from %s ||| Video duration => %s]]]', video_file, duration)
-    ttl = duration // 2
-    thumb_image_path = path or os.path.join(Config.DOWN_PATH, f"{basename(video_file)}.jpg")
-    command = f"ffmpeg -ss {ttl} -i '{video_file}' -vframes 1 '{thumb_image_path}'"
-    err = (await runcmd(command))[1]
-    if err:
-        _LOG.error(err)
-    return thumb_image_path if os.path.exists(thumb_image_path) else None
