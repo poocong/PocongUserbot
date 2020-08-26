0ef1a601a24fba2c01451813c8beba05b71d63cb
X Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-08-26 11:30:35 +0700

    Update user_id.py

--- userbot/modules/user_id.py
+++ userbot/modules/user_id.py
@@ -1,49 +1,38 @@
-from asyncio.exceptions import TimeoutError
-
+from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-
-from userbot import CMD_HELP, bot
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from userbot.events import register
+from userbot import bot, CMD_HELP
 
-
-@register(outgoing=True, pattern=r"^\.gid(?: |$)(.*)")
-async def firstname(uid):
-    if uid.fwd_from:
+@register(outgoing=True, pattern="^\.gid(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
         return 
-    if not uid.reply_to_msg_id:
-       await uid.edit("```Balas di Pesan Goblok!!.```")
+    if not event.reply_to_msg_id:
+       await event.edit("`Balas Di Teks Ajg!!`")
        return
-    message = await uid.get_reply_message() 
+    reply_message = await event.get_reply_message() 
+    if not reply_message.text:
+       await event.edit("```Balas Di Teks Goblok!!```")
+       return
     chat = "@getidsbot"
-    user_id = message.sender.id
-    id = f"/search_id {user_id}"
     sender = reply_message.sender
     if reply_message.sender.bot:
-       await uid.edit("```Balas di Pesan Goblok!!.```")
+       await event.edit("`Balas Di Teks Asu!!`")
        return
-    await uid.edit("```Membongkar ID..```")
-    async with borg.conversation(chat) as conv:
-          try:
-                msg = await conv.send_message(id)
-                r = await conv.get_response()
-                response = await conv.get_response()
-            except YouBlockedUserError:
-                await uid.reply("`Bunuh @getidsbot dulu bos, biar botnya bisa jalan -_-`")
-                return
-          if response.text.startswith("Profil Buriq Tidak Punya ID,"):
-             await uid.edit("```Profil Buriq Tidak Punya ID..```")
-          await uid.client.delete_messages(
-                    conv.chat_id, [msg.id, r.id, response.id]
-                )
-                return
-            else:
-                respond = await conv.get_response()
-                await uid.edit(f"{response.message}")
-            await uid.client.delete_messages(
-                conv.chat_id, [msg.id, r.id, response.id, respond.id]
-            )
-    except TimeoutError:
-        return await event.edit("`Error: `@getidsbot not responding!.`")
+    await event.edit("`Proses Melacak Pikiran Tersangka.......`")
+    async with bot.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
+              await bot.forward_messages(chat, reply_message)
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("`Bunuh @getidsbot dulu bos, biar botnya bisa jalan -_-`")
+              return
+          if response.text.startswith("Forward"):
+             await event.edit("`Njerrr Profile Tidak Diketahui/Buriq..`")
+          else: 
+             await event.edit(f"{response.message.message}")
 
 
 CMD_HELP.update({
