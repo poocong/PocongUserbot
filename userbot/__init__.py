cf25df8d98697dfb79628cd8f5bc7e03de5a6a21
X Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-08-27 21:39:37 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -356,13 +356,9 @@ def paginate_help(page_number, loaded_modules, prefix):
                     "⌫️", data="{}_prev({})".format(prefix, modulo_page)
                 ),
                 custom.Button.inline(
-                    "☠️", data="{}_exit({})".format(prefix, modulo_page)
-                ),
-                custom.Button.inline(
                     "⌦️", data="{}_next({})".format(prefix, modulo_page)
                 )
             )
-         )
         ]
     return pairs
 
