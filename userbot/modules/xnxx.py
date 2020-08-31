c646f13c9648de643eabde5f838e66a6e5e60b1c
X Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-08-31 21:06:52 +0700

    Update xnxx.py

--- userbot/modules/xnxx.py
+++ userbot/modules/xnxx.py
@@ -36,4 +36,4 @@ async def _(event):
             await event.reply("```Unblock @XiaomiGeeksBot plox```")
         else:
             await event.delete()
-            await bot.forward_messages(event.chat_id)
+            await bot.forward_messages(event.chat_id, response.message.message)
