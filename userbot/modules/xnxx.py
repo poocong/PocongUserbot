1138490b1b183b7571a9627e3f73488fe369665c
X Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-08-31 19:49:01 +0700

    Update xnxx.py

--- userbot/modules/xnxx.py
+++ userbot/modules/xnxx.py
@@ -37,4 +37,4 @@ async def _(event):
             return
         else:
             await event.delete()
-            await event.forward_messages(event.chat_id, response.message)
+            await messages.edit(event.chat_id, response.message)
