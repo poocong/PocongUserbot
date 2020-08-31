ca50e9fe4ae6fcf955d95f462a31b9e2ac8a2236
X Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-08-31 18:31:02 +0700

    Asu

---
+++ userbot/modules/xnxx.py
@@ -0,0 +1,30 @@
+# created by @eve_enryu
+
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from userbot import bot
+from userbot.events import register
+
+
+@register(outgoing=True, pattern=r"^\.o(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    link = event.pattern_match.group(1)
+    chat = "@SaitamaRobot"
+    wall = f"wall"
+    await event.edit("```Processing```")
+    async with bot.conversation("@SaitamaRobot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(
+                    incoming=True,
+                    from_users=701937965))
+            await conv.send_message(f'/{wall} {link}')
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("```Unblock @XiaomiGeeksBot plox```")
+            return
+        else:
+            await event.delete()
+            await bot.forward_messages(event.chat_id, response.message)
