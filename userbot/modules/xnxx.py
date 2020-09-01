a6aad2981a917072e03480797e555ae29279db9c
X Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-09-01 19:02:34 +0700

    Xnxx

--- userbot/modules/xnxx.py
+++ userbot/modules/xnxx.py
@@ -8,20 +8,20 @@ from userbot.events import register
 async def _(event):
     if event.fwd_from:
         return
-    link = event.pattern_match.group(1)
-    chat = "@SaitamaRobot"
+    link = event.pattern_match.group(2)
+    chat = "@xbotgroup_xbot"
     wall = f"wall"
     await event.edit("```Processing```")
-    async with bot.conversation("@SaitamaRobot") as conv:
+    async with bot.conversation("@xbotgroup_xbot") as conv:
         try:
             response = conv.wait_event(
                 events.NewMessage(
                     incoming=True,
-                    from_users=701937965))
+                    from_users=1281618755))
             await conv.send_message(f'/{wall} {link}')
             response = await response
         except YouBlockedUserError:
-            await event.reply("```Unblock @SaitamaRobot plox```")
+            await event.reply("```Unblock @xbotgroup_xbot plox```")
         else:
             await event.delete()
             await bot.forward_messages(event.chat_id, response.message)
