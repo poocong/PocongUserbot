d672bd8215bd3c892fd9a0a794b0b289703580d5
X Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-09-01 20:32:12 +0700

    Update xnxx.py

--- userbot/modules/xnxx.py
+++ userbot/modules/xnxx.py
@@ -1,9 +1,8 @@
-# created by @eve_enryu
-
-from telethon import events
+import os
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from userbot import bot
+
 from userbot.events import register
+from userbot import TEMP_DOWNLOAD_DIRECTORY, bot
 
 
 @register(outgoing=True, pattern=r"^\.o(?: |$)(.*)")
@@ -16,14 +15,39 @@ async def _(event):
     await event.edit("```Processing```")
     async with bot.conversation("@SaitamaRobot") as conv:
         try:
+            msg = await conv.send_message(now)
             response = conv.wait_event(
                 events.NewMessage(
                     incoming=True,
                     from_users=701937965))
             await conv.send_message(f'/{wall} {link}')
-            response = await response
+            """ - don't spam notif - """
+            await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await event.reply("```Unblock @XiaomiGeeksBot plox```")
+            await event.reply("`Please unblock` @SpotifyNowBot`...`")
+            return
+        if response.text.startswith("You're"):
+            await event.edit("`You're not listening to anything on Spotify at the moment`")
+            await event.client.delete_messages(conv.chat_id,
+                                               [msg.id, response.id])
+            return
+        if response.text.startswith("Ads."):
+            await event.edit("`You're listening to those annoying ads.`")
+            await event.client.delete_messages(conv.chat_id,
+                                               [msg.id, response.id])
             return
         else:
-            await event.client.send_file(event.chat_id)
+            downloaded_file_name = await event.client.download_media(
+                response.media,
+                TEMP_DOWNLOAD_DIRECTORY
+            )
+            await event.client.send_file(
+                event.chat_id,
+                downloaded_file_name,
+                force_document=False,
+            )
+            """ - cleanup chat after completed - """
+            await event.client.delete_messages(conv.chat_id,
+                                               [msg.id, response.id])
+    await event.delete()
+    return os.remove(downloaded_file_name)
