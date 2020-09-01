3f99606317a7cf513ccac10a4198be8e4e0db18f
X Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-09-01 20:12:57 +0700

    Asu

--- userbot/modules/xnxx.py
+++ userbot/modules/xnxx.py
@@ -1,11 +1,7 @@
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from userbot import bot
 from userbot.events import register
-from asyncio.exceptions import TimeoutError
-from PIL import Image
-from io import BytesIO
-from userbot.events import register
+from userbot import TEMP_DOWNLOAD_DIRECTORY, bot
 
 
 @register(outgoing=True, pattern=r"^\.o(?: |$)(.*)")
@@ -13,7 +9,6 @@ async def _(event):
     if event.fwd_from:
         return
     link = event.pattern_match.group(1)
-    chat = "@xbotgroup_xbot"
     wall = f"wall"
     await event.edit("```Processing```")
     async with bot.conversation("@xbotgroup_xbot") as conv:
@@ -26,6 +21,27 @@ async def _(event):
             response = await response
         except YouBlockedUserError:
             await event.reply("```Unblock @xbotgroup_xbot plox```")
+            return
+        if response.text.startswith("Hi"):
+            await walpe.edit("`Please do /start` @xbotgroup_xbot`...`")
         else:
-            await event.client.send_file(event.chat_id, respone.message)
-            
+            downloaded_file_name = await event.client.download_media(
+                response.media, TEMP_DOWNLOAD_DIRECTORY
+            )
+            await event.client.send_file(
+                event.chat_id,
+                downloaded_file_name,
+                force_document=False,
+                reply_to=message_id_to_reply,
+            )
+            """ - cleanup chat after completed - """
+            try:
+                msg_query
+            except NameError:
+                await event.client.delete_messages(conv.chat_id, [msg.id, response.id])
+            else:
+                await event.client.delete_messages(
+                    conv.chat_id, [msg.id, response.id, r.id, msg_query.id]
+                )
+    await event.delete()
+    return os.remove(downloaded_file_name)
