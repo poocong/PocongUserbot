cc7225cd00b74890ad91b1e3d221f28763caf619
X Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-09-01 21:00:16 +0700

    Update xnxx.py

--- userbot/modules/xnxx.py
+++ userbot/modules/xnxx.py
@@ -21,19 +21,8 @@ async def _(event):
                     from_users=1281618755))
             await conv.send_message(f'/{wall} {link}')
             response = await response
-            await bot.send_read_acknowledge(conv.chat_id)
-        except YouBlockedUserError:
-            await event.reply("`Unblock` @xbotgroup_xbot dulu Goblok!!`...`")
-            return
-        if response.text.startswith("You're"):
-            await event.edit("`You're not listening to anything on Spotify at the moment`")
-            await event.client.delete_messages(conv.chat_id,
-                                               [msg.id, response.id])
-            return
-        if response.text.startswith("Ads."):
-            await event.edit("`You're listening to those annoying ads.`")
-            await event.client.delete_messages(conv.chat_id,
-                                               [msg.id, response.id])
+            except YouBlockedUserError:
+            await event.edit("```Unblock @xbotgroup_xbot dulu Goblok!!```")
             return
         else:
             downloaded_file_name = await event.client.download_media(
