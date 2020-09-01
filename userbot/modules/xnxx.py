bf3773be232057cb2e5d67981747fd0caf58f81b
X Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-09-01 20:18:38 +0700

    Update xnxx.py

--- userbot/modules/xnxx.py
+++ userbot/modules/xnxx.py
@@ -1,47 +1,49 @@
-from telethon import events
+import os
 from telethon.errors.rpcerrorlist import YouBlockedUserError
+
 from userbot.events import register
 from userbot import TEMP_DOWNLOAD_DIRECTORY, bot
 
 
-@register(outgoing=True, pattern=r"^\.o(?: |$)(.*)")
+@register(outgoing=True, pattern=r'^\.o(:? |$)(.*)?')
 async def _(event):
     if event.fwd_from:
         return
-    link = event.pattern_match.group(1)
-    wall = f"wall"
-    await event.edit("```Processing```")
-    async with bot.conversation("@xbotgroup_xbot") as conv:
+    event.pattern_match.group(1)
+    chat = "@xbotgroup_xbot"
+    now = f"/wall kimi no na wa"
+    await event.edit("`Processing...`")
+    async with event.client.conversation(chat) as conv:
         try:
-            response = conv.wait_event(
-                events.NewMessage(
-                    incoming=True,
-                    from_users=1281618755))
-            await conv.send_message(f'/{wall} {link}')
-            response = await response
+            msg = await conv.send_message(now)
+            response = await conv.get_response()
+            """ - don't spam notif - """
+            await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await event.reply("```Unblock @xbotgroup_xbot plox```")
+            await event.reply("`Please unblock` @xbotgroup_xbot`...`")
             return
-        if response.text.startswith("Hi"):
-            await walpe.edit("`Please do /start` @xbotgroup_xbot`...`")
+        if response.text.startswith("You're"):
+            await event.edit("`You're not listening to anything on Spotify at the moment`")
+            await event.client.delete_messages(conv.chat_id,
+                                               [msg.id, response.id])
+            return
+        if response.text.startswith("Ads."):
+            await event.edit("`You're listening to those annoying ads.`")
+            await event.client.delete_messages(conv.chat_id,
+                                               [msg.id, response.id])
+            return
         else:
             downloaded_file_name = await event.client.download_media(
-                response.media, TEMP_DOWNLOAD_DIRECTORY
+                response.media,
+                TEMP_DOWNLOAD_DIRECTORY
             )
             await event.client.send_file(
                 event.chat_id,
                 downloaded_file_name,
                 force_document=False,
-                reply_to=message_id_to_reply,
             )
             """ - cleanup chat after completed - """
-            try:
-                msg_query
-            except NameError:
-                await event.client.delete_messages(conv.chat_id, [msg.id, response.id])
-            else:
-                await event.client.delete_messages(
-                    conv.chat_id, [msg.id, response.id, r.id, msg_query.id]
-                )
+            await event.client.delete_messages(conv.chat_id,
+                                               [msg.id, response.id])
     await event.delete()
     return os.remove(downloaded_file_name)
