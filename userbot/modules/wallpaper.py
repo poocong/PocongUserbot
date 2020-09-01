80b4f818f6ffe2d574586d0711edc43727617ef0
X Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-09-01 22:16:46 +0700

    Update wallpaper.py

--- userbot/modules/wallpaper.py
+++ userbot/modules/wallpaper.py
@@ -16,18 +16,6 @@ down_p = str(TEMP_DOWNLOAD_DIRECTORY.rstrip("/"))
 async def wallp(event):
     if not os.path.isdir(down_p):
         os.makedirs(down_p)
-chat_id = event.effective_chat.id
-    msg = event.effective_message
-    args = context.args
-    msg_id = event.effective_message.message_id
-    event = context.event
-    query = " ".join(args)
-    if not query:
-        msg.edit_text("Please enter a query!")
-        return
-    else:
-        caption = query
-        term = query.replace(" ", "%20")
         json_rep = r.get(
             f"https://wall.alphacoders.com/api2.0/get.php?auth={WALL_API}&method=search&term={term}"
         ).json()
@@ -46,16 +34,12 @@ chat_id = event.effective_chat.id
                 event.send_photo(
                     chat_id,
                     photo=wallpaper,
-                    caption='Preview',
-                    reply_to_message_id=msg_id,
-                    timeout=60)
+                    caption='Preview')
                 event.send_document(
                     chat_id,
                     document=wallpaper,
                     filename='wallpaper',
-                    caption=caption,
-                    reply_to_message_id=msg_id,
-                    timeout=60)
+                    caption=caption)
 
 
 CMD_HELP.update({"wallpaper": ">`.wall` <query>."
