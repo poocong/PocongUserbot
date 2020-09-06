bf493eda18ea8ec4ec4aa2d8d640f2fcc64ef5e7
X Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-09-06 18:55:54 +0700

    Add files via upload

---
+++ userbot/modules/mirror.py
@@ -0,0 +1,71 @@
+import asyncio
+import io
+import os
+import random
+import re
+import textwrap
+from asyncio.exceptions import TimeoutError
+from random import randint, uniform
+
+from glitch_this import ImageGlitcher
+from PIL import Image, ImageDraw, ImageEnhance, ImageFont, ImageOps
+from telethon import events, functions, types
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.types import DocumentAttributeFilename
+
+from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY, bot
+from userbot.events import register
+
+Converted = TEMP_DOWNLOAD_DIRECTORY + "sticker.webp"
+
+@register(outgoing=True, pattern=r"^\.mirror(?: |$)(.*)")
+async def glitch(event):
+    if not event.reply_to_msg_id:
+        await event.edit("`Balas di Sticker Goblok!!!`")
+        return
+    reply_message = await event.get_reply_message()
+    if not reply_message.media:
+        await event.edit("`Balas di Sticker Goblok!!!`")
+        return
+    await bot.download_file(reply_message.media)
+    await event.edit("`Processing....`")
+    if event.is_reply:
+        data = await check_media(reply_message)
+        if isinstance(data, bool):
+            await event.edit("`File tidak di dukung...`")
+            return
+    else:
+        await event.edit("`Reply to Any Media Sur`")
+        return
+
+    try:
+        value = int(event.pattern_match.group(1))
+        if value > 9:
+            raise ValueError
+    except ValueError:
+        value = 2
+    await event.edit("```Mirror This Media```")
+    await asyncio.sleep(2)
+    file_name = "mirror.png"
+    to_download_directory = TEMP_DOWNLOAD_DIRECTORY
+    downloaded_file_name = os.path.join(to_download_directory, file_name)
+    downloaded_file_name = await bot.download_media(
+        reply_message,
+        downloaded_file_name,
+    )
+    mirror_flip_file = downloaded_file_name
+    mirror = ImageMirror()
+    mirror_img = mirror.open(mirror_flip_file).convert('RGB')
+        IMG = ImageOps.mirror(mirror_img)
+    else:
+    IMG.save(Converted, quality=95)
+    await event.edit("`Uploading Mirror Media...`")
+    await event.client.send_file(
+        event.chat_id, sticker=Converted, force_document=False, reply_to=event.reply_to_msg_id
+    )
+    await event.delete()
+    os.remove(sticker=Converted)
+    
+    
+    
+    
