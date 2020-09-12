fb84d41b7680bf97107bc5a0146d47fd3cac21b6
XDark-AngeL <70883062+XDark-AngeL@users.noreply.github.com>
2020-09-13 00:03:38 +0700

    Update music.py

--- userbot/modules/music.py
+++ userbot/modules/music.py
@@ -1,8 +1,4 @@
-# Originally from Bothub
-# Port to UserBot by @heyworld
-#Copyright (C) 2020 azrim.
-
-
+import asyncio
 import glob
 import os
 import shutil
@@ -15,6 +11,7 @@ from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from pylast import User
 from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.types import DocumentAttributeAudio, DocumentAttributeVideo
 
 from userbot import (
@@ -27,24 +24,6 @@ from userbot import (
 )
 from userbot.events import register
 from userbot.utils import progress
-import asyncio
-#from userbot.utils import admin_cmd
-from userbot.events import register 
-from telethon.errors.rpcerrorlist import YouBlockedUserError
-try:
- import subprocess
-except:
- os.system("pip install instantmusic")
- 
-
-
-os.system("rm -rf *.mp3")
-
-
-def bruh(name):
-    
-    os.system("instantmusic -q -s "+name)
-    
 
 # For getvideosong
 
@@ -68,26 +47,46 @@ def getmusicvideo(cat):
     os.system(command)
 
 
-
-@register(outgoing=True, pattern="^.spd(?: |$)(.*)")
+@register(outgoing=True, pattern=r"^\.netease (?:(now)|(.*) - (.*))")
 async def _(event):
     if event.fwd_from:
         return
-    link = event.pattern_match.group(1)
-    chat = "@SpotifyMusicDownloaderBot"
-    await event.edit("```Getting Your Music```")
-    async with bot.conversation(chat) as conv:
-          await asyncio.sleep(2)
-          await event.edit("`Downloading music taking some times,  Stay Tuned.....`")
-          try:
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=752979930))
-              await bot.send_message(chat, link)
-              respond = await response
-          except YouBlockedUserError:
-              await event.reply("```Please unblock @SpotifyMusicDownloaderBot and try again```")
-              return
-          await event.delete()
-          await bot.forward_messages(event.chat_id, respond.message)
+    if event.pattern_match.group(1) == "now":
+        playing = User(LASTFM_USERNAME, lastfm).get_now_playing()
+        if playing is None:
+            return await event.edit("`Error: No current scrobble found.`")
+        artist = playing.get_artist()
+        song = playing.get_title()
+    else:
+        artist = event.pattern_match.group(2)
+        song = event.pattern_match.group(3)
+    track = str(artist) + " - " + str(song)
+    chat = "@WooMaiBot"
+    link = f"/netease {track}"
+    await event.edit("`Searching...`")
+    try:
+        async with bot.conversation(chat) as conv:
+            await asyncio.sleep(2)
+            await event.edit("`Processing... Please wait`")
+            try:
+                msg = await conv.send_message(link)
+                response = await conv.get_response()
+                respond = await conv.get_response()
+                await bot.send_read_acknowledge(conv.chat_id)
+            except YouBlockedUserError:
+                await event.reply("`Please unblock @WooMaiBot and try again`")
+                return
+            await event.edit("`Sending Your Music...`")
+            await asyncio.sleep(3)
+            await bot.send_file(event.chat_id, respond)
+        await event.client.delete_messages(
+            conv.chat_id, [msg.id, response.id, respond.id]
+        )
+        await event.delete()
+    except TimeoutError:
+        return await event.edit(
+            "`Error: `@WooMaiBot` is not responding or Song not found!.`"
+        )
 
 
 @register(outgoing=True, pattern=r"^\.vsong(?: |$)(.*)")
@@ -155,77 +154,187 @@ async def _(event):
     os.system("rm -rf *.mp4")
     os.system("rm -rf *.webm")
 
-@register(outgoing=True, pattern="^.netease(?: |$)(.*)")
-async def WooMai(netase):
-    if netase.fwd_from:
+
+@register(outgoing=True, pattern=r"^\.smd(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
         return
-    song = netase.pattern_match.group(1)
-    chat = "@WooMaiBot"
-    link = f"/netease {song}"
-    await netase.edit("```Getting Your Music```")
+    link = event.pattern_match.group(1)
+    chat = "@SpotifyMusicDownloaderBot"
+    await event.edit("```Getting Your Music```")
     async with bot.conversation(chat) as conv:
-          await asyncio.sleep(2)
-          await netase.edit("`Downloading...Please wait`")
-          try:
-              msg = await conv.send_message(link)
-              response = await conv.get_response()
-              respond = await conv.get_response()
-              """ - don't spam notif - """
-              await bot.send_read_acknowledge(conv.chat_id)
-          except YouBlockedUserError:
-              await netase.reply("```Please unblock @WooMaiBot and try again```")
-              return
-          await netase.edit("`Sending Your Music...`")
-          await asyncio.sleep(3)
-          await bot.send_file(netase.chat_id, respond)
-    await netase.client.delete_messages(conv.chat_id,
-                                       [msg.id, response.id, respond.id])
-    await netase.delete()
+        await asyncio.sleep(2)
+        await event.edit("`Downloading music taking some times,  Stay Tuned.....`")
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=752979930)
+            )
+            msg = await bot.send_message(chat, link)
+            respond = await response
+            res = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=752979930)
+            )
+            r = await res
+            await bot.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await event.reply(
+                "```Please unblock @SpotifyMusicDownloaderBot and try again```"
+            )
+            return
+        await bot.forward_messages(event.chat_id, respond.message)
+    await event.client.delete_messages(conv.chat_id, [msg.id, r.id, respond.id])
+    await event.delete()
 
 
-@register(outgoing=True, pattern="^.dzd(?: |$)(.*)")
-async def DeezLoader(Deezlod):
-    if Deezlod.fwd_from:
+@register(outgoing=True, pattern=r"^\.deez (.+?|) (FLAC|MP3\_320|MP3\_256|MP3\_128)")
+async def _(event):
+    if event.fwd_from:
         return
-    d_link = Deezlod.pattern_match.group(1)
-    if ".com" not in d_link:
-        await Deezlod.edit("` I need a link to download something pro.`**(._.)**")
+
+    strings = {
+        "name": "DeezLoad",
+        "arl_token_cfg_doc": "ARL Token for Deezer",
+        "invalid_arl_token": "please set the required variables for this module",
+        "wrong_cmd_syntax": "bruh, now i think how far should we go. please terminate my Session �朮",
+        "server_error": "We're experiencing technical difficulties.",
+        "processing": "`Downloading..`",
+        "uploading": "`Uploading...`",
+    }
+
+    ARL_TOKEN = DEEZER_ARL_TOKEN
+
+    if ARL_TOKEN is None:
+        await event.edit(strings["invalid_arl_token"])
+        return
+
+    try:
+        loader = deezloader.Login(ARL_TOKEN)
+    except Exception as er:
+        await event.edit(str(er))
+        return
+
+    temp_dl_path = os.path.join(TEMP_DOWNLOAD_DIRECTORY, str(time.time()))
+    if not os.path.exists(temp_dl_path):
+        os.makedirs(temp_dl_path)
+
+    required_link = event.pattern_match.group(1)
+    required_qty = event.pattern_match.group(2)
+
+    await event.edit(strings["processing"])
+
+    if "spotify" in required_link:
+        if "track" in required_link:
+            required_track = loader.download_trackspo(
+                required_link,
+                output=temp_dl_path,
+                quality=required_qty,
+                recursive_quality=True,
+                recursive_download=True,
+                not_interface=True,
+            )
+            await event.edit(strings["uploading"])
+            await upload_track(required_track, event)
+            shutil.rmtree(temp_dl_path)
+            await event.delete()
+
+        elif "album" in required_link:
+            reqd_albums = loader.download_albumspo(
+                required_link,
+                output=temp_dl_path,
+                quality=required_qty,
+                recursive_quality=True,
+                recursive_download=True,
+                not_interface=True,
+                zips=False,
+            )
+            for required_track in reqd_albums:
+                await event.edit(strings["uploading"])
+                await upload_track(required_track, event)
+            shutil.rmtree(temp_dl_path)
+            await event.delete()
+
+    elif "deezer" in required_link:
+        if "track" in required_link:
+            required_track = loader.download_trackdee(
+                required_link,
+                output=temp_dl_path,
+                quality=required_qty,
+                recursive_quality=True,
+                recursive_download=True,
+                not_interface=True,
+            )
+            await upload_track(required_track, event)
+            await event.edit(strings["uploading"])
+            shutil.rmtree(temp_dl_path)
+            await event.delete()
+
+        elif "album" in required_link:
+            reqd_albums = loader.download_albumdee(
+                required_link,
+                output=temp_dl_path,
+                quality=required_qty,
+                recursive_quality=True,
+                recursive_download=True,
+                not_interface=True,
+                zips=False,
+            )
+            for required_track in reqd_albums:
+                await event.edit(strings["uploading"])
+                await upload_track(required_track, event)
+            shutil.rmtree(temp_dl_path)
+            await event.delete()
+
     else:
-        await Deezlod.edit("**Initiating Download!**")
-    chat = "@DeezLoadBot"
-    async with bot.conversation(chat) as conv:
-          try:
-              msg_start = await conv.send_message("/start")
-              response = await conv.get_response()
-              r = await conv.get_response()
-              msg = await conv.send_message(d_link)
-              details = await conv.get_response()
-              song = await conv.get_response()
-              """ - don't spam notif - """
-              await bot.send_read_acknowledge(conv.chat_id)
-          except YouBlockedUserError:
-              await Deezlod.edit("**Error:** `unblock` @DeezLoadBot `and retry!`")
-              return
-          await bot.send_file(Deezlod.chat_id, song, caption=details.text)
-          await Deezlod.client.delete_messages(conv.chat_id,
-                                             [msg_start.id, response.id, r.id, msg.id, details.id, song.id])
-          await Deezlod.delete()          
-    
-CMD_HELP.update({
-        "music":
-        ".spd`<Artist - Song Title>\
-            \nUsage:For searching songs from Spotify.\
-            \n\n`.netease` <Artist - Song Title>\
-            \nUsage:Download music with @WooMaiBot\
-            \n\n`.vsong` **Artist - Song Title**\
-            \nUsage: Finding and uploading videoclip.\
-            \n\n`.dzd` <Spotify/Deezer Link>\
-            \nUsage:Download music from Spotify or Deezer.\
-            \n\n`.deezload` <spotify/deezer link> <Format>\
-            \nUsage: Download music from deezer.\
-            \n\n Well deezer is not available in India so create an deezer account using vpn. Set DEEZER_ARL_TOKEN in vars to make this work.\
-            \n\n *Format= `FLAC`, `MP3_320`, `MP3_256`, `MP3_128`.\
-            \n\n\n Guide:Video guide of arl token: [here](https://www.youtube.com/watch?v=O6PRT47_yds&feature=youtu.be) or Read [This](https://notabug.org/RemixDevs/DeezloaderRemix/wiki/Login+via+userToken)."
-})
+        await event.edit(strings["wrong_cmd_syntax"])
 
 
+async def upload_track(track_location, message):
+    metadata = extractMetadata(createParser(track_location))
+    duration = 0
+    title = ""
+    performer = ""
+    if metadata.has("duration"):
+        duration = metadata.get("duration").seconds
+    if metadata.has("title"):
+        title = metadata.get("title")
+    if metadata.has("artist"):
+        performer = metadata.get("artist")
+    document_attributes = [
+        DocumentAttributeAudio(
+            duration=duration,
+            voice=False,
+            title=title,
+            performer=performer,
+            waveform=None,
+        )
+    ]
+    supports_streaming = True
+    force_document = False
+    caption_rts = os.path.basename(track_location)
+    await message.client.send_file(
+        message.chat_id,
+        track_location,
+        caption=caption_rts,
+        force_document=force_document,
+        supports_streaming=supports_streaming,
+        allow_cache=False,
+        attributes=document_attributes,
+    )
+    os.remove(track_location)
+
+
+CMD_HELP.update(
+    {
+        "music": ">`.netease <Artist - Song Title>`"
+        "\nUsage: Download music with @WooMaiBot"
+        "\n\n>`.netease now`"
+        "\nUsage: Download current LastFM scrobble use `@WooMaiBot`."
+        "\n\n>`.vsong` **Artist - Song Title**"
+        "\nUsage: Finding and uploading videoclip."
+        "\n\n>`.smd <Artist - Song Title>`"
+        "\nUsage: Download music from Spotify"
+        "\n\n>`.deez <spotify/deezer link> FORMAT`"
+        "\nUsage: Download music from deezer."
+        "\n*Format : `FLAC`, `MP3_320`, `MP3_256`, `MP3_128`."
+    }
+)
