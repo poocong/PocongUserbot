cf75ba747f2782fa757532362e5fde057a63fb39
XDark-AngeL <70883062+XDark-AngeL@users.noreply.github.com>
2020-09-12 23:53:46 +0700

    Update music.py

--- userbot/modules/music.py
+++ userbot/modules/music.py
@@ -1,4 +1,231 @@
-#Encript Marshal By XVenom
-#https://github.com/xvenom15
-import marshal
-exec(marshal.loads(b'\xe3\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x06\x00\x00\x00@\x00\x00\x00s\xc8\x00\x00\x00d\x00d\x01l\x00m\x01Z\x01\x01\x00d\x00d\x02l\x02Z\x02d\x00d\x03l\x03m\x04Z\x04\x01\x00d\x00d\x04l\x05m\x06Z\x06m\x07Z\x07\x01\x00d\x00d\x05l\x08m\tZ\t\x01\x00d\x00d\x02l\nZ\nz\x0cd\x00d\x02l\x0bZ\x0bW\x00n\x16\x01\x00\x01\x00\x01\x00e\n\xa0\x0cd\x06\xa1\x01\x01\x00Y\x00n\x02X\x00e\n\xa0\x0cd\x07\xa1\x01\x01\x00d\x08d\t\x84\x00Z\re\x04d\nd\x0bd\x0c\x8d\x02d\rd\x0e\x84\x00\x83\x01Z\x0ee\x04d\nd\x0fd\x0c\x8d\x02d\x10d\x11\x84\x00\x83\x01Z\x0fe\x04d\nd\x12d\x0c\x8d\x02d\x13d\x14\x84\x00\x83\x01Z\x10e\x07\xa0\x11d\x15d\x16i\x01\xa1\x01\x01\x00d\x02S\x00)\x17\xe9\x00\x00\x00\x00)\x01\xda\x06eventsN)\x01\xda\x08register)\x02\xda\x03bot\xda\x08CMD_HELP)\x01\xda\x13YouBlockedUserErrorz\x18pip install instantmusicz\x0crm -rf *.mp3c\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x04\x00\x00\x00C\x00\x00\x00s\x12\x00\x00\x00t\x00\xa0\x01d\x01|\x00\x17\x00\xa1\x01\x01\x00d\x00S\x00)\x02Nz\x13instantmusic -q -s )\x02\xda\x02os\xda\x06system)\x01\xda\x04name\xa9\x00r\n\x00\x00\x00\xda\x00\xda\x04bruh\x16\x00\x00\x00s\x02\x00\x00\x00\x00\x02r\x0c\x00\x00\x00Tz\x10^.spd(?: |$)(.*))\x02Z\x08outgoingZ\x07patternc\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x06\x00\x00\x00\n\x00\x00\x00\xc3\x00\x00\x00s\x02\x01\x00\x00|\x00j\x00r\nd\x00S\x00|\x00j\x01\xa0\x02d\x01\xa1\x01}\x01d\x02}\x02|\x00\xa0\x03d\x03\xa1\x01I\x00d\x00H\x00\x01\x00t\x04\xa0\x05|\x02\xa1\x014\x00I\x00d\x00H\x00\x9a\xb6}\x03t\x06\xa0\x07d\x04\xa1\x01I\x00d\x00H\x00\x01\x00|\x00\xa0\x03d\x05\xa1\x01I\x00d\x00H\x00\x01\x00z4|\x03\xa0\x08t\tj\nd\x06d\x07d\x08\x8d\x02\xa1\x01}\x04t\x04\xa0\x0b|\x02|\x01\xa1\x02I\x00d\x00H\x00\x01\x00|\x04I\x00d\x00H\x00}\x05W\x00n6\x04\x00t\x0ck\nr\xc8\x01\x00\x01\x00\x01\x00|\x00\xa0\rd\t\xa1\x01I\x00d\x00H\x00\x01\x00Y\x00W\x005\x00Q\x00I\x00d\x00H\x00R\x00\xa3\x00d\x00S\x00X\x00|\x00\xa0\x0e\xa1\x00I\x00d\x00H\x00\x01\x00t\x04\xa0\x0f|\x00j\x10|\x05j\x11\xa1\x02I\x00d\x00H\x00\x01\x00W\x005\x00Q\x00I\x00d\x00H\x00R\x00X\x00d\x00S\x00)\nN\xe9\x01\x00\x00\x00z\x1a@SpotifyMusicDownloaderBot\xfa\x18```Getting Your Music```\xe9\x02\x00\x00\x00z7`Downloading music taking some times,  Stay Tuned.....`Ti\xda\x8f\xe1,)\x02Z\x08incomingZ\nfrom_usersz=```Please unblock @SpotifyMusicDownloaderBot and try again```)\x12\xda\x08fwd_from\xda\rpattern_match\xda\x05group\xda\x04editr\x04\x00\x00\x00\xda\x0cconversation\xda\x07asyncio\xda\x05sleepZ\nwait_eventr\x02\x00\x00\x00Z\nNewMessage\xda\x0csend_messager\x06\x00\x00\x00\xda\x05reply\xda\x06deleteZ\x10forward_messages\xda\x07chat_id\xda\x07message)\x06Z\x05event\xda\x04link\xda\x04chat\xda\x04conv\xda\x08response\xda\x07respondr\n\x00\x00\x00r\n\x00\x00\x00r\x0b\x00\x00\x00\xda\x01_\x1c\x00\x00\x00s"\x00\x00\x00\x00\x02\x06\x01\x04\x01\x0c\x01\x04\x01\x10\x01\x14\x01\x10\x01\x10\x01\x02\x01\x14\x01\x12\x01\x0e\x01\x0e\x01\x10\x01\x18\x01\x0e\x01r!\x00\x00\x00z\x14^.netease(?: |$)(.*)c\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x08\x00\x00\x00\n\x00\x00\x00\xc3\x00\x00\x00sZ\x01\x00\x00|\x00j\x00r\nd\x00S\x00|\x00j\x01\xa0\x02d\x01\xa1\x01}\x01d\x02}\x02d\x03|\x01\x9b\x00\x9d\x02}\x03|\x00\xa0\x03d\x04\xa1\x01I\x00d\x00H\x00\x01\x00t\x04\xa0\x05|\x02\xa1\x014\x00I\x00d\x00H\x00\x9a\xd4}\x04t\x06\xa0\x07d\x05\xa1\x01I\x00d\x00H\x00\x01\x00|\x00\xa0\x03d\x06\xa1\x01I\x00d\x00H\x00\x01\x00zB|\x04\xa0\x08|\x03\xa1\x01I\x00d\x00H\x00}\x05|\x04\xa0\t\xa1\x00I\x00d\x00H\x00}\x06|\x04\xa0\t\xa1\x00I\x00d\x00H\x00}\x07t\x04\xa0\n|\x04j\x0b\xa1\x01I\x00d\x00H\x00\x01\x00W\x00n6\x04\x00t\x0ck\nr\xe0\x01\x00\x01\x00\x01\x00|\x00\xa0\rd\x07\xa1\x01I\x00d\x00H\x00\x01\x00Y\x00W\x005\x00Q\x00I\x00d\x00H\x00R\x00\xa3\x00d\x00S\x00X\x00|\x00\xa0\x03d\x08\xa1\x01I\x00d\x00H\x00\x01\x00t\x06\xa0\x07d\t\xa1\x01I\x00d\x00H\x00\x01\x00t\x04\xa0\x0e|\x00j\x0b|\x07\xa1\x02I\x00d\x00H\x00\x01\x00W\x005\x00Q\x00I\x00d\x00H\x00R\x00X\x00|\x00j\x0f\xa0\x10|\x04j\x0b|\x05j\x11|\x06j\x11|\x07j\x11g\x03\xa1\x02I\x00d\x00H\x00\x01\x00|\x00\xa0\x12\xa1\x00I\x00d\x00H\x00\x01\x00d\x00S\x00)\nNr\r\x00\x00\x00z\n@WooMaiBotz\t/netease r\x0e\x00\x00\x00r\x0f\x00\x00\x00z\x1b`Downloading...Please wait`z-```Please unblock @WooMaiBot and try again```z\x17`Sending Your Music...`\xe9\x03\x00\x00\x00)\x13r\x10\x00\x00\x00r\x11\x00\x00\x00r\x12\x00\x00\x00r\x13\x00\x00\x00r\x04\x00\x00\x00r\x14\x00\x00\x00r\x15\x00\x00\x00r\x16\x00\x00\x00r\x17\x00\x00\x00\xda\x0cget_response\xda\x15send_read_acknowledger\x1a\x00\x00\x00r\x06\x00\x00\x00r\x18\x00\x00\x00\xda\tsend_file\xda\x06client\xda\x0fdelete_messages\xda\x02idr\x19\x00\x00\x00)\x08Z\x06netase\xda\x04songr\x1d\x00\x00\x00r\x1c\x00\x00\x00r\x1e\x00\x00\x00\xda\x03msgr\x1f\x00\x00\x00r \x00\x00\x00r\n\x00\x00\x00r\n\x00\x00\x00r\x0b\x00\x00\x00\xda\x06WooMai0\x00\x00\x00s0\x00\x00\x00\x00\x02\x06\x01\x04\x01\x0c\x01\x04\x01\n\x01\x10\x01\x14\x01\x10\x01\x10\x01\x02\x01\x10\x01\x0e\x01\x0e\x02\x16\x01\x0e\x01\x10\x01\x18\x01\x10\x01\x10\x01$\x01\n\x01\x0e\xff\n\x02r+\x00\x00\x00z\x10^.dzd(?: |$)(.*)c\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\n\x00\x00\x00\n\x00\x00\x00\xc3\x00\x00\x00sl\x01\x00\x00|\x00j\x00r\nd\x00S\x00|\x00j\x01\xa0\x02d\x01\xa1\x01}\x01d\x02|\x01k\x07r0|\x00\xa0\x03d\x03\xa1\x01I\x00d\x00H\x00\x01\x00n\x10|\x00\xa0\x03d\x04\xa1\x01I\x00d\x00H\x00\x01\x00d\x05}\x02t\x04\xa0\x05|\x02\xa1\x014\x00I\x00d\x00H\x00\x90\x01\x9a\x04}\x03zn|\x03\xa0\x06d\x06\xa1\x01I\x00d\x00H\x00}\x04|\x03\xa0\x07\xa1\x00I\x00d\x00H\x00}\x05|\x03\xa0\x07\xa1\x00I\x00d\x00H\x00}\x06|\x03\xa0\x06|\x01\xa1\x01I\x00d\x00H\x00}\x07|\x03\xa0\x07\xa1\x00I\x00d\x00H\x00}\x08|\x03\xa0\x07\xa1\x00I\x00d\x00H\x00}\tt\x04\xa0\x08|\x03j\t\xa1\x01I\x00d\x00H\x00\x01\x00W\x00n8\x04\x00t\nk\n\x90\x01r\x00\x01\x00\x01\x00\x01\x00|\x00\xa0\x03d\x07\xa1\x01I\x00d\x00H\x00\x01\x00Y\x00W\x005\x00Q\x00I\x00d\x00H\x00R\x00\xa3\x00d\x00S\x00X\x00t\x04j\x0b|\x00j\t|\t|\x08j\x0cd\x08\x8d\x03I\x00d\x00H\x00\x01\x00|\x00j\r\xa0\x0e|\x03j\t|\x04j\x0f|\x05j\x0f|\x06j\x0f|\x07j\x0f|\x08j\x0f|\tj\x0fg\x06\xa1\x02I\x00d\x00H\x00\x01\x00|\x00\xa0\x10\xa1\x00I\x00d\x00H\x00\x01\x00W\x005\x00Q\x00I\x00d\x00H\x00R\x00X\x00d\x00S\x00)\tNr\r\x00\x00\x00z\x04.comz4` I need a link to download something pro.`**(._.)**z\x18**Initiating Download!**z\x0c@DeezLoadBotz\x06/startz.**Error:** `unblock` @DeezLoadBot `and retry!`)\x01Z\x07caption)\x11r\x10\x00\x00\x00r\x11\x00\x00\x00r\x12\x00\x00\x00r\x13\x00\x00\x00r\x04\x00\x00\x00r\x14\x00\x00\x00r\x17\x00\x00\x00r#\x00\x00\x00r$\x00\x00\x00r\x1a\x00\x00\x00r\x06\x00\x00\x00r%\x00\x00\x00\xda\x04textr&\x00\x00\x00r\'\x00\x00\x00r(\x00\x00\x00r\x19\x00\x00\x00)\nZ\x07DeezlodZ\x06d_linkr\x1d\x00\x00\x00r\x1e\x00\x00\x00Z\tmsg_startr\x1f\x00\x00\x00\xda\x01rr*\x00\x00\x00Z\x07detailsr)\x00\x00\x00r\n\x00\x00\x00r\n\x00\x00\x00r\x0b\x00\x00\x00\xda\nDeezLoaderL\x00\x00\x00s0\x00\x00\x00\x00\x02\x06\x01\x04\x01\x0c\x01\x08\x01\x12\x02\x10\x01\x04\x01\x16\x01\x02\x01\x10\x01\x0e\x01\x0e\x01\x10\x01\x0e\x01\x0e\x02\x16\x01\x10\x01\x10\x01\x18\x01\x1a\x01\n\x01\x1a\xff\n\x02r.\x00\x00\x00Z\x05musica\x0e\x03\x00\x00.spd`<Artist - Song Title>            \nUsage:For searching songs from Spotify.            \n\n`.netease` <Artist - Song Title>            \nUsage:Download music with @WooMaiBot            \n\n`.dzd` <Spotify/Deezer Link>            \nUsage:Download music from Spotify or Deezer.            \n\n`.deezload` <spotify/deezer link> <Format>            \nUsage: Download music from deezer.            \n\n Well deezer is not available in India so create an deezer account using vpn. Set DEEZER_ARL_TOKEN in vars to make this work.            \n\n *Format= `FLAC`, `MP3_320`, `MP3_256`, `MP3_128`.            \n\n\n Guide:Video guide of arl token: [here](https://www.youtube.com/watch?v=O6PRT47_yds&feature=youtu.be) or Read [This](https://notabug.org/RemixDevs/DeezloaderRemix/wiki/Login+via+userToken).)\x12Z\x08telethonr\x02\x00\x00\x00r\x15\x00\x00\x00Z\x0euserbot.eventsr\x03\x00\x00\x00Z\x07userbotr\x04\x00\x00\x00r\x05\x00\x00\x00Z\x1ctelethon.errors.rpcerrorlistr\x06\x00\x00\x00r\x07\x00\x00\x00\xda\nsubprocessr\x08\x00\x00\x00r\x0c\x00\x00\x00r!\x00\x00\x00r+\x00\x00\x00r.\x00\x00\x00\xda\x06updater\n\x00\x00\x00r\n\x00\x00\x00r\n\x00\x00\x00r\x0b\x00\x00\x00\xda\x08<module>\x05\x00\x00\x00s*\x00\x00\x00\x0c\x01\x08\x02\x0c\x01\x10\x01\x0c\x01\x08\x01\x02\x01\x0c\x01\x06\x01\x10\x04\n\x03\x08\x06\n\x01\n\x13\n\x01\n\x1b\n\x01\n\x1b\x04\x01\x02\x01\x02\xfe'))
+# Originally from Bothub
+# Port to UserBot by @heyworld
+#Copyright (C) 2020 azrim.
+
+
+import glob
+import os
+import shutil
+import time
+
+import deezloader
+import requests
+from bs4 import BeautifulSoup
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+from pylast import User
+from telethon import events
+from telethon.tl.types import DocumentAttributeAudio, DocumentAttributeVideo
+
+from userbot import (
+    CMD_HELP,
+    DEEZER_ARL_TOKEN,
+    LASTFM_USERNAME,
+    TEMP_DOWNLOAD_DIRECTORY,
+    bot,
+    lastfm,
+)
+from userbot.events import register
+from userbot.utils import progress
+import asyncio
+#from userbot.utils import admin_cmd
+from userbot.events import register 
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+try:
+ import subprocess
+except:
+ os.system("pip install instantmusic")
+ 
+
+
+os.system("rm -rf *.mp3")
+
+
+def bruh(name):
+    
+    os.system("instantmusic -q -s "+name)
+    
+
+# For getvideosong
+
+
+def getmusicvideo(cat):
+    search = cat
+    headers = {
+        "User-Agent": "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+    }
+    html = requests.get(
+        "https://www.youtube.com/results?search_query=" + search, headers=headers
+    ).text
+    soup = BeautifulSoup(html, "html.parser")
+    for link in soup.find_all("a"):
+        if "/watch?v=" in link.get("href"):
+            # May change when Youtube Website may get updated in the future.
+            video_link = link.get("href")
+            break
+    video_link = "http://www.youtube.com/" + video_link
+    command = 'youtube-dl -f "[filesize<50M]" --merge-output-format mp4 ' + video_link
+    os.system(command)
+
+
+
+@register(outgoing=True, pattern="^.spd(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    link = event.pattern_match.group(1)
+    chat = "@SpotifyMusicDownloaderBot"
+    await event.edit("```Getting Your Music```")
+    async with bot.conversation(chat) as conv:
+          await asyncio.sleep(2)
+          await event.edit("`Downloading music taking some times,  Stay Tuned.....`")
+          try:
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=752979930))
+              await bot.send_message(chat, link)
+              respond = await response
+          except YouBlockedUserError:
+              await event.reply("```Please unblock @SpotifyMusicDownloaderBot and try again```")
+              return
+          await event.delete()
+          await bot.forward_messages(event.chat_id, respond.message)
+
+
+@register(outgoing=True, pattern=r"^\.vsong(?: |$)(.*)")
+async def _(event):
+    reply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    reply = await event.get_reply_message()
+    if event.pattern_match.group(1):
+        query = event.pattern_match.group(1)
+        await event.edit("`Wait..! I am finding your videosong..`")
+    elif reply.message:
+        query = reply.message
+        await event.edit("`Wait..! I am finding your videosong..`")
+    else:
+        await event.edit("`What I am Supposed to find?`")
+        return
+    getmusicvideo(query)
+    l = glob.glob(("*.mp4")) + glob.glob(("*.mkv")) + glob.glob(("*.webm"))
+    if l:
+        await event.edit("`Yeah..! i found something..`")
+    else:
+        await event.edit(f"Sorry..! i can't find anything with `{query}`")
+    loa = l[0]
+    metadata = extractMetadata(createParser(loa))
+    duration = 0
+    width = 0
+    height = 0
+    if metadata.has("duration"):
+        duration = metadata.get("duration").seconds
+    if metadata.has("width"):
+        width = metadata.get("width")
+    if metadata.has("height"):
+        height = metadata.get("height")
+    await event.edit("`Uploading video.. Please wait..`")
+    os.system("cp *mp4 thumb.mp4")
+    os.system("ffmpeg -i thumb.mp4 -vframes 1 -an -s 480x360 -ss 5 thumb.jpg")
+    thumb_image = "thumb.jpg"
+    c_time = time.time()
+    await event.client.send_file(
+        event.chat_id,
+        loa,
+        force_document=False,
+        thumb=thumb_image,
+        allow_cache=False,
+        caption=query,
+        supports_streaming=True,
+        reply_to=reply_to_id,
+        attributes=[
+            DocumentAttributeVideo(
+                duration=duration,
+                w=width,
+                h=height,
+                round_message=False,
+                supports_streaming=True,
+            )
+        ],
+        progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+            progress(d, t, event, c_time, "[UPLOAD]", loa)
+        ),
+    )
+    await event.delete()
+    os.remove(thumb_image)
+    os.system("rm -rf *.mkv")
+    os.system("rm -rf *.mp4")
+    os.system("rm -rf *.webm")
+
+@register(outgoing=True, pattern="^.netease(?: |$)(.*)")
+async def WooMai(netase):
+    if netase.fwd_from:
+        return
+    song = netase.pattern_match.group(1)
+    chat = "@WooMaiBot"
+    link = f"/netease {song}"
+    await netase.edit("```Getting Your Music```")
+    async with bot.conversation(chat) as conv:
+          await asyncio.sleep(2)
+          await netase.edit("`Downloading...Please wait`")
+          try:
+              msg = await conv.send_message(link)
+              response = await conv.get_response()
+              respond = await conv.get_response()
+              """ - don't spam notif - """
+              await bot.send_read_acknowledge(conv.chat_id)
+          except YouBlockedUserError:
+              await netase.reply("```Please unblock @WooMaiBot and try again```")
+              return
+          await netase.edit("`Sending Your Music...`")
+          await asyncio.sleep(3)
+          await bot.send_file(netase.chat_id, respond)
+    await netase.client.delete_messages(conv.chat_id,
+                                       [msg.id, response.id, respond.id])
+    await netase.delete()
+
+
+@register(outgoing=True, pattern="^.dzd(?: |$)(.*)")
+async def DeezLoader(Deezlod):
+    if Deezlod.fwd_from:
+        return
+    d_link = Deezlod.pattern_match.group(1)
+    if ".com" not in d_link:
+        await Deezlod.edit("` I need a link to download something pro.`**(._.)**")
+    else:
+        await Deezlod.edit("**Initiating Download!**")
+    chat = "@DeezLoadBot"
+    async with bot.conversation(chat) as conv:
+          try:
+              msg_start = await conv.send_message("/start")
+              response = await conv.get_response()
+              r = await conv.get_response()
+              msg = await conv.send_message(d_link)
+              details = await conv.get_response()
+              song = await conv.get_response()
+              """ - don't spam notif - """
+              await bot.send_read_acknowledge(conv.chat_id)
+          except YouBlockedUserError:
+              await Deezlod.edit("**Error:** `unblock` @DeezLoadBot `and retry!`")
+              return
+          await bot.send_file(Deezlod.chat_id, song, caption=details.text)
+          await Deezlod.client.delete_messages(conv.chat_id,
+                                             [msg_start.id, response.id, r.id, msg.id, details.id, song.id])
+          await Deezlod.delete()          
+    
+CMD_HELP.update({
+        "music":
+        ".spd`<Artist - Song Title>\
+            \nUsage:For searching songs from Spotify.\
+            \n\n`.netease` <Artist - Song Title>\
+            \nUsage:Download music with @WooMaiBot\
+            \n\n>`.vsong` **Artist - Song Title**"
+            \nUsage: Finding and uploading videoclip.\
+            \n\n`.dzd` <Spotify/Deezer Link>\
+            \nUsage:Download music from Spotify or Deezer.\
+            \n\n`.deezload` <spotify/deezer link> <Format>\
+            \nUsage: Download music from deezer.\
+            \n\n Well deezer is not available in India so create an deezer account using vpn. Set DEEZER_ARL_TOKEN in vars to make this work.\
+            \n\n *Format= `FLAC`, `MP3_320`, `MP3_256`, `MP3_128`.\
+            \n\n\n Guide:Video guide of arl token: [here](https://www.youtube.com/watch?v=O6PRT47_yds&feature=youtu.be) or Read [This](https://notabug.org/RemixDevs/DeezloaderRemix/wiki/Login+via+userToken)."
+})
+
+
