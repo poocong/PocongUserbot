519415aaeea7e4bc3c56cbd1504c01c3de211427
XDark-AngeL <70883062+XDark-AngeL@users.noreply.github.com>
2020-09-20 16:38:17 +0700

    Up Version

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -26,7 +26,7 @@ except PhoneNumberInvalidError:
 for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
-LOGS.info("You are running XBOT-REMIX version: [2.1]")
+LOGS.info("You are running on XBOT-REMIX version: [3.0]")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
