ba4cc1b13fc7cde6fd70d13428516f199f31b9e7
X Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-09-23 01:40:31 +0700

    Update music.py

--- userbot/modules/music.py
+++ userbot/modules/music.py
@@ -149,6 +149,7 @@ async def _(event):
             ),
         )
         await event.edit(f"**{query}** `Uploaded Successfully..!`")
+        await event.delete()
         os.remove(thumb_image)
         os.system("rm -rf *.mkv")
         os.system("rm -rf *.mp4")
