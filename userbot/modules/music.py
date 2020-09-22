101c14531ca62994d8b466e3f28b548776e8dd75
X Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-09-23 01:09:08 +0700

    Woi

--- userbot/modules/music.py
+++ userbot/modules/music.py
@@ -96,63 +96,69 @@ async def _(event):
     reply = await event.get_reply_message()
     if event.pattern_match.group(1):
         query = event.pattern_match.group(1)
-        await event.edit("`Mencari Music Video..`")
+        await event.edit("`Mencari Music Video...`")
     elif reply:
         query = str(reply.message)
         await event.edit("`Mencari Music Video..`")
     else:
-        await event.edit("`Apa yang harus saya cari?`")
+        await event.edit("`Apa Yang Harus Saya Cari?`")
         return
     await getmusicvideo(query)
     l = glob.glob(("*.mp4")) + glob.glob(("*.mkv")) + glob.glob(("*.webm"))
     if l:
-        await event.edit("`Music Video Ditemukan..`")
+        await event.edit("`OK..! Music Video Ditemukan..`")
     else:
-        await event.edit(f"`Maaf..! Saya tidak menemukan` **{query}**")
+        await event.edit(f"`Sorry..! Saya tidak dapat menemukan` **{query}**")
         return
-    loa = l[0]
-    metadata = extractMetadata(createParser(loa))
-    duration = 0
-    width = 0
-    height = 0
-    if metadata.has("duration"):
-        duration = metadata.get("duration").seconds
-    if metadata.has("width"):
-        width = metadata.get("width")
-    if metadata.has("height"):
-        height = metadata.get("height")
-    os.system("cp *mp4 thumb.mp4")
-    os.system("ffmpeg -i thumb.mp4 -vframes 1 -an -s 480x360 -ss 5 thumb.jpg")
-    thumb_image = "thumb.jpg"
-    c_time = time.time()
-    await event.client.send_file(
-        event.chat_id,
-        loa,
-        force_document=False,
-        thumb=thumb_image,
-        allow_cache=False,
-        caption=query,
-        supports_streaming=True,
-        reply_to=reply_to_id,
-        attributes=[
-            DocumentAttributeVideo(
-                duration=duration,
-                w=width,
-                h=height,
-                round_message=False,
-                supports_streaming=True,
-            )
-        ],
-        progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-            progress(d, t, event, c_time, "[UPLOAD]", loa)
-        ),
-    )
-    await event.edit(f"**{query}** `Uploaded Successfully..!`")
-    await event.delete()
-    os.remove(thumb_image)
-    os.system("rm -rf *.mkv")
-    os.system("rm -rf *.mp4")
-    os.system("rm -rf *.webm")
+    try:
+        loa = l[0]
+        metadata = extractMetadata(createParser(loa))
+        duration = 0
+        width = 0
+        height = 0
+        if metadata.has("duration"):
+            duration = metadata.get("duration").seconds
+        if metadata.has("width"):
+            width = metadata.get("width")
+        if metadata.has("height"):
+            height = metadata.get("height")
+        os.system("cp *mp4 thumb.mp4")
+        os.system("ffmpeg -i thumb.mp4 -vframes 1 -an -s 480x360 -ss 5 thumb.jpg")
+        thumb_image = "thumb.jpg"
+        c_time = time.time()
+        await event.client.send_file(
+            event.chat_id,
+            loa,
+            force_document=False,
+            thumb=thumb_image,
+            allow_cache=False,
+            caption=query,
+            supports_streaming=True,
+            reply_to=reply_to_id,
+            attributes=[
+                DocumentAttributeVideo(
+                    duration=duration,
+                    w=width,
+                    h=height,
+                    round_message=False,
+                    supports_streaming=True,
+                )
+            ],
+            progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                progress(d, t, event, c_time, "[UPLOAD]", loa)
+            ),
+        )
+        await event.edit(f"**{query}** `Uploaded Successfully..!`")
+        os.remove(thumb_image)
+        os.system("rm -rf *.mkv")
+        os.system("rm -rf *.mp4")
+        os.system("rm -rf *.webm")
+    except BaseException:
+        os.remove(thumb_image)
+        os.system("rm -rf *.mkv")
+        os.system("rm -rf *.mp4")
+        os.system("rm -rf *.webm")
+        return
 
 @register(outgoing=True, pattern=r"^\.smd(?: |$)(.*)")
 async def _(event):
