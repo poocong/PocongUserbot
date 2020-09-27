3c328a03ae43d1fef5b26225eaa1bc840b4c06bb
X-Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-09-27 23:22:20 +0700

    Update string_session.py

--- string_session.py
+++ string_session.py
@@ -12,16 +12,16 @@ print("""Please go-to my.telegram.org
 Login using your Telegram account
 Click on API Development Tools
 Create a new application, by entering the required details
-Check your Telegram saved messages section to copy the STRING_SESSION""")
+Check di Telegram Pesan Tersimpan untuk Melihat Kode STRING_SESSION""")
 API_KEY = int(input("Enter API_KEY here: "))
 API_HASH = input("Enter API_HASH here: ")
 
 with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
-    print("Check your Telegram Saved Messages to copy the STRING_SESSION value")
+    print("Check di Telegram Pesan Tersimpan untuk Melihat Kode STRING_SESSION")
     session_string = client.session.save()
-    saved_messages_template = """Support: @PPE_support
+    saved_messages_template = """XBOT CHANNEL SUPPORT: @XBOT_SUPPORT
 
 <code>STRING_SESSION</code>: <code>{}</code>
 
-⚠️ <i>Please be careful before passing this value to third parties</i>""".format(session_string)
+⚠️ <i>Silahkan Klick Code String Untuk Mengcopy</i>""".format(session_string)
     client.send_message("me", saved_messages_template, parse_mode="html")
