c065b4ad8d6932835eda88e4d1476e1b82763870
X-Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-09-27 22:57:49 +0700

    Update __init__.py

--- userbot/utils/__init__.py
+++ userbot/utils/__init__.py
@@ -2,9 +2,5 @@ from .chrome import chrome, options
 from .progress import progress
 from .google_images_download import googleimagesdownload
 
-from .tools import (
-    humanbytes,
-    time_formatter,
-    human_to_bytes,
-    md5
-)
+from .tools import humanbytes, time_formatter, human_to_bytes, md5, check_media
+
