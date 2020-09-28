8479d3ebb613e5e6d118d6922ecf0864b3c112d8
X-Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-09-28 20:21:31 +0700

    Update stickers_v2.py

--- userbot/modules/stickers_v2.py
+++ userbot/modules/stickers_v2.py
@@ -58,31 +58,17 @@ async def _(event):
                     incoming=True,
                     from_users=611085086))
             msg = await event.client.forward_messages(chat, reply_message)
+            response = await response
             await bot.send_read_acknowledge(conv.chat_id)
-            response = await response
         except YouBlockedUserError:
-            await event.reply("unblock me (@stickers_to_image_bot) to work")
+            await event.reply("unblock me (@stickers_to_image_bot) and try again")
             return
-        if response.text.startswith("I understand only stickers"):
-            await event.edit("Sorry i cant't convert it check wheter is non animated sticker or not")
+        if response.text.startswith("Hi!"):
+            await event.edit("Gagal!!!")
         else:
-            response = conv.wait_event(
-                events.NewMessage(
-                    incoming=True,
-                    from_users=611085086))
-            response = await response
-            if response.text.startswith("..."):
-                response = conv.wait_event(
-                    events.NewMessage(
-                        incoming=True,
-                        from_users=611085086))
-                response = await response
-                await bot.send_read_acknowledge(conv.chat_id)
-                await event.delete()
-                await event.client.send_message(event.chat_id, response.message, reply_to=reply_message.id)
-            else:
-                await event.edit("try again")
-                await event.client.delete_messages(conv.chat_id, [msg.id, response.id])
+            await event.delete()
+            await event.client.send_message(event.chat_id, response.message, reply_to=reply_message.id)   
+            await event.client.delete_messages(conv.chat_id, [msg.id, response.id])
 
 
 @register(outgoing=True, pattern="^.stoi$")
