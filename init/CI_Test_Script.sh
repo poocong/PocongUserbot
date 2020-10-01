61a6a5d3ea7bf8f7bf4b20b97e9318caaffc809c
X-Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-10-01 12:45:59 +0700

    Add files via upload

--- init/CI_Test_Script.sh
+++ init/CI_Test_Script.sh
@@ -13,13 +13,13 @@ export SEMAPHORE_PROJECT_DIR=`pwd`
 . "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"/telegram
 TELEGRAM_TOKEN=${BOT_API_KEY}
 export BOT_API_KEY TELEGRAM_TOKEN
-tg_sendinfo "<code>I am gonna merge staging into x-sql-extended</code>"
+tg_sendinfo "<code>I am gonna merge staging into sql-extended</code>"
 cd
-git clone https://github.com/X-Newbie/XBot-Remix.git
-cd XBot-Remix
-git remote set-url origin https://${GH_USERNAME}:${GH_PERSONAL_TOKEN}@github.com/X-Newbie/XBot-Remix.git
+git clone https://github.com/mkaraniya/OpenUserBot.git
+cd OpenUserBot
+git remote set-url origin https://${GH_USERNAME}:${GH_PERSONAL_TOKEN}@github.com/mkaraniya/OpenUserBot.git
 git fetch
 git checkout staging
 git pull origin staging
-git push --force origin staging:x-sql-extended
-tg_sendinfo "<code>I have merged all commits from staging into x-sql-extended</code>"
+git push --force origin staging:sql-extended
+tg_sendinfo "<code>I have merged all commits from staging into sql-extended</code>"
