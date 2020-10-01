7234dd5725990e560dadad004992de2e88ff686e
X-Newbie <sxnewbie@gmail.com>
2020-10-01 08:49:24 +0000

    pylint: auto fixes

--- userbot/modules/sangmata.py
+++ userbot/modules/sangmata.py
@@ -5,7 +5,6 @@
 #
 # Port to userbot by @MoveAngel
 
-from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from userbot import bot, CMD_HELP
 from userbot.events import register
@@ -64,7 +63,7 @@ async def lastname(steal):
 
 
 CMD_HELP.update({
-        "sangmata": 
+    "sangmata":
         "`.sg`\
           \nUsage: Steal ur or friend name."
 })
