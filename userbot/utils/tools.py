669bf237157291dd0efe63246e1085fe4ff24d5b
X-Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-10-02 22:26:26 +0700

    Update tools.py

--- userbot/utils/tools.py
+++ userbot/utils/tools.py
@@ -88,22 +88,6 @@ async def is_admin(chat_id, user_id):
     return False
 
 
-async def take_screen_shot(video_file: str, duration: int, path: str = '') -> Optional[str]:
-    """ take a screenshot """
-    LOGS.info(
-        '[[[Extracting a frame from %s ||| Video duration => %s]]]',
-        video_file,
-        duration)
-    ttl = duration // 2
-    thumb_image_path = path or os.path.join(
-        "./temp/", f"{basename(video_file)}.jpg")
-    command = f"ffmpeg -ss {ttl} -i '{video_file}' -vframes 1 '{thumb_image_path}'"
-    err = (await runcmd(command))[1]
-    if err:
-        LOGS.error(err)
-    return thumb_image_path if os.path.exists(thumb_image_path) else None
-
-
 async def check_media(reply_message):
     if reply_message and reply_message.media:
         if reply_message.photo:
