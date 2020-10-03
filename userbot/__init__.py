44f485b913c8da9a8c3b788d24188c289ae3f3d7
X-Newbie <sxnewbie@gmail.com>
2020-10-03 11:56:53 +0000

    pylint: auto fixes

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -2,7 +2,7 @@
 #
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
-#inline credit @keselekpermen69
+# inline credit @keselekpermen69
 """ Userbot initialization. """
 
 import os
