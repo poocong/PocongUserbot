38099e87d4d0da274731ed4f47bed7dd05dfef50
X-Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-10-04 06:35:43 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -1,44 +1,37 @@
 # Copyright (C) 2019 The Raphielscape Company LLC.
 #
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
 #
-""" Userbot help command """
 
 import asyncio
+
 from userbot import CMD_HELP
 from userbot.events import register
 
-modules = CMD_HELP
 
-
-@register(outgoing=True, pattern="^.help(?: |$)(.*)")
-async def help(event):
-    """ For .help command,"""
+@register(outgoing=True, pattern=r"^\.help(?: |$)(.*)")
+async def hep(event):
     args = event.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
             await event.edit(str(CMD_HELP[args]))
+            await asyncio.sleep(15)
+            await event.delete()
         else:
-            await event.edit("**Module Salah Goblokkkk!!**")
-            await asyncio.sleep(18)
+            await event.edit("Please specify a valid module name.")
+            await asyncio.sleep(5)
             await event.delete()
     else:
-        await event.edit(f"**╭━━━━━━━━━━━━━━━━━━━━━╮**\
-            \n│   Help for [🔥XBOT-REMIX🔥]\
-            \n│   ╾────────────────╼ \
-            \n│   Untuk melihat lengkap Command\
-            \n│   Contoh: .help <nama module>\
-            \n│   Modules Aktif: {len(modules)}\
-           \n╰━━━━━━━━━━━━━━━━━━━━━╯")
-
-        string = ""
+        string1 = "Please specify which module do you want help for !!\nUsage: .help <module name>\n\n"
+        string = "• "
+        string3 = "List for all available commands below: "
+        string2 = "-------------------------------------------------------------"
         for i in CMD_HELP:
             string += "`" + str(i)
-            string += "`\t𖠄\t "
-        await event.reply(f"{string}"
-                          "\n\n╰━┉┄═━┉┄═━┉✫┄═━═━┉┄═━╯\
-                          \n**Mod By**'➳͜͡❂ঔৣ⃕͜x͠N͜͡ᎬᎳᏴᏆᎬ࿐'")
+            string += "`  •  "
+        await event.edit(
+            f"{string1}" f"{string3}" f"{string2}\n" f"{string}" f"{string2}"
+        )
         await asyncio.sleep(30)
         await event.delete()
-        await event.delete.string()
