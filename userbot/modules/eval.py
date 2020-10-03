b6aa9769102aa1d16b8510a43d43a1cd673b4331
X-Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-10-03 19:43:04 +0700

    Update eval.py

--- userbot/modules/eval.py
+++ userbot/modules/eval.py
@@ -1,34 +1,29 @@
 # Copyright (C) 2019 The Raphielscape Company LLC.
 #
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
 #
-""" Userbot module for executing code and terminal commands from Telegram. """
 
 import asyncio
-from getpass import getuser
 from os import remove
 from sys import executable
-from userbot import CMD_HELP, BOTLOG, BOTLOG_CHATID
+
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, TERM_ALIAS
 from userbot.events import register
 
 
-@register(outgoing=True, pattern="^.eval(?: |$)(.*)")
+@register(outgoing=True, pattern=r"^\.eval(?: |$)(.*)")
 async def evaluate(query):
-    """ For .eval command, evaluates the given Python expression. """
     if query.is_channel and not query.is_group:
-        await query.edit("`Eval isn't permitted on channels`")
-        return
+        return await query.edit("`Eval isn't permitted on channels`")
 
     if query.pattern_match.group(1):
         expression = query.pattern_match.group(1)
     else:
-        await query.edit("``` Give an expression to evaluate. ```")
-        return
+        return await query.edit("``` Give an expression to evaluate. ```")
 
     if expression in ("userbot.session", "config.env"):
-        await query.edit("`That's a dangerous operation! Not Permitted!`")
-        return
+        return await query.edit("`That's a dangerous operation! Not Permitted!`")
 
     try:
         evaluation = str(eval(expression))
@@ -46,62 +41,64 @@ async def evaluate(query):
                     )
                     remove("output.txt")
                     return
-                await query.edit("**Query: **\n`"
-                                 f"{expression}"
-                                 "`\n**Result: **\n`"
-                                 f"{evaluation}"
-                                 "`")
+                await query.edit(
+                    "**Query: **\n`"
+                    f"{expression}"
+                    "`\n**Result: **\n`"
+                    f"{evaluation}"
+                    "`"
+                )
         else:
-            await query.edit("**Query: **\n`"
-                             f"{expression}"
-                             "`\n**Result: **\n`No Result Returned/False`")
+            await query.edit(
+                "**Query: **\n`"
+                f"{expression}"
+                "`\n**Result: **\n`No Result Returned/False`"
+            )
     except Exception as err:
-        await query.edit("**Query: **\n`"
-                         f"{expression}"
-                         "`\n**Exception: **\n"
-                         f"`{err}`")
+        await query.edit(
+            "**Query: **\n`" f"{expression}" "`\n**Exception: **\n" f"`{err}`"
+        )
 
     if BOTLOG:
         await query.client.send_message(
-            BOTLOG_CHATID,
-            f"Eval query {expression} was executed successfully")
+            BOTLOG_CHATID, f"Eval query {expression} was executed successfully"
+        )
 
 
-@register(outgoing=True, pattern=r"^.exec(?: |$)([\s\S]*)")
+@register(outgoing=True, pattern=r"^\.exec(?: |$)([\s\S]*)")
 async def run(run_q):
-    """ For .exec command, which executes the dynamically created program """
     code = run_q.pattern_match.group(1)
 
     if run_q.is_channel and not run_q.is_group:
-        await run_q.edit("`Exec isn't permitted on channels!`")
-        return
+        return await run_q.edit("`Exec isn't permitted on channels!`")
 
     if not code:
-        await run_q.edit("``` At least a variable is required to \
-execute. Use .help exec for an example.```")
-        return
+        return await run_q.edit(
+            "``` At least a variable is required to"
+            "execute. Use .help exec for an example.```"
+        )
 
     if code in ("userbot.session", "config.env"):
-        await run_q.edit("`That's a dangerous operation! Not Permitted!`")
-        return
+        return await run_q.edit("`That's a dangerous operation! Not Permitted!`")
 
     if len(code.splitlines()) <= 5:
         codepre = code
     else:
         clines = code.splitlines()
-        codepre = clines[0] + "\n" + clines[1] + "\n" + clines[2] + \
-            "\n" + clines[3] + "..."
+        codepre = (
+            clines[0] + "\n" + clines[1] + "\n" + clines[2] + "\n" + clines[3] + "..."
+        )
 
     command = "".join(f"\n {l}" for l in code.split("\n.strip()"))
     process = await asyncio.create_subprocess_exec(
         executable,
-        '-c',
+        "-c",
         command.strip(),
         stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE)
+        stderr=asyncio.subprocess.PIPE,
+    )
     stdout, stderr = await process.communicate()
-    result = str(stdout.decode().strip()) \
-        + str(stderr.decode().strip())
+    result = str(stdout.decode().strip()) + str(stderr.decode().strip())
 
     if result:
         if len(result) > 4096:
@@ -116,53 +113,47 @@ execute. Use .help exec for an example.```")
             )
             remove("output.txt")
             return
-        await run_q.edit("**Query: **\n`"
-                         f"{codepre}"
-                         "`\n**Result: **\n`"
-                         f"{result}"
-                         "`")
+        await run_q.edit(
+            "**Query: **\n`" f"{codepre}" "`\n**Result: **\n`" f"{result}" "`"
+        )
     else:
-        await run_q.edit("**Query: **\n`"
-                         f"{codepre}"
-                         "`\n**Result: **\n`No Result Returned/False`")
+        await run_q.edit(
+            "**Query: **\n`" f"{codepre}" "`\n**Result: **\n`No Result Returned/False`"
+        )
 
     if BOTLOG:
         await run_q.client.send_message(
-            BOTLOG_CHATID,
-            "Exec query " + codepre + " was executed successfully")
+            BOTLOG_CHATID, "Exec query " + codepre + " was executed successfully"
+        )
 
 
-@register(outgoing=True, pattern="^.term(?: |$)(.*)")
+@register(outgoing=True, pattern=r"^\.term(?: |$)(.*)")
 async def terminal_runner(term):
-    """ For .term command, runs bash commands and scripts on your server. """
-    curruser = getuser()
+    curruser = TERM_ALIAS
     command = term.pattern_match.group(1)
     try:
         from os import geteuid
+
         uid = geteuid()
     except ImportError:
         uid = "This ain't it chief!"
 
     if term.is_channel and not term.is_group:
-        await term.edit("`Term commands aren't permitted on channels!`")
-        return
+        return await term.edit("`Term commands aren't permitted on channels!`")
 
     if not command:
-        await term.edit("``` Give a command or use .help hacker for \
-            an example.```")
-        return
+        return await term.edit(
+            "``` Give a command or use .help term for an example.```"
+        )
 
     if command in ("userbot.session", "config.env"):
-        await term.edit("`That's a dangerous operation! Not Permitted!`")
-        return
+        return await term.edit("`That's a dangerous operation! Not Permitted!`")
 
-    process = await asyncio.create_subprocess_exec(
-        command,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE)
+    process = await asyncio.create_subprocess_shell(
+        command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
     stdout, stderr = await process.communicate()
-    result = str(stdout.decode().strip()) \
-        + str(stderr.decode().strip())
+    result = str(stdout.decode().strip()) + str(stderr.decode().strip())
 
     if len(result) > 4096:
         output = open("output.txt", "w+")
@@ -182,34 +173,20 @@ async def terminal_runner(term):
     else:
         await term.edit("`" f"{curruser}:~$ {command}" f"\n{result}" "`")
 
+
+"""
     if BOTLOG:
         await term.client.send_message(
             BOTLOG_CHATID,
             "Terminal Command " + command + " was executed sucessfully",
         )
+"""
 
-
-CMD_HELP.update({
-    "hacker":
-    "`.eval` 2+3\
-\nUsage: Evalute mini-expressions.\
-\n\n`.exec` print('hello')\
-\nusage: Execute small python scripts.\
-\n\n`.term` ls\
-\nUsage: Run bash commands and scripts on your server.\
-\n\n`.w3m google.com`\
-\nUsage: Browse the internet with w3m on your server.\nPut your device into landscape mode for better preview."
-})
-
-
-CMD_HELP.update({
-    "hacker":
-    "`.eval` 2+3\
-\nUsage: Evalute mini-expressions.\
-\n\n`.exec` print('hello')\
-\nusage: Execute small python scripts.\
-\n\n`.term` ls\
-\nUsage: Run bash commands and scripts on your server.\
-\n\n`.w3m google.com`\
-\nUsage: Browse the internet with w3m on your server.\nPut your device into landscape mode for better preview."
-})
+CMD_HELP.update(
+    {
+        "eval": ">`.eval 2 + 3`" "\nUsage: Evalute mini-expressions.",
+        "exec": ">`.exec print('hello')`" "\nUsage: Execute small python scripts.",
+        "term": ">`.term <cmd>`"
+        "\nUsage: Run bash commands and scripts on your server.",
+    }
+)
