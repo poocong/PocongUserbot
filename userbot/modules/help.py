e3f3cf84e9d387a2237af54cdb34f9d84509e163
X-Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-10-04 08:29:44 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -1,40 +1,43 @@
 # Copyright (C) 2019 The Raphielscape Company LLC.
 #
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
+""" Userbot help command """
 
 import asyncio
-
 from userbot import CMD_HELP
 from userbot.events import register
+from userbot.utils import defender_kanger
 
+modules = CMD_HELP
 
-@register(outgoing=True, pattern=r"^\.help(?: |$)(.*)")
-async def hep(event):
+@register(outgoing=True, pattern="^.help(?: |$)(.*)")
+async def help(event):
+    """ For .help command,"""
     args = event.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
             await event.edit(str(CMD_HELP[args]))
-            await asyncio.sleep(15)
-            await event.delete()
         else:
             await event.edit("**Module Salah Goblokkkk!!**")
-            await asyncio.sleep(5)
+            await asyncio.sleep(18)
             await event.delete()
     else:
-        string1 = "╭━━━━━━━━━━━━━━━━━━━━╮\n│    Help for [🔥XBOT - REMIX🔥]   │\
-            \n╰━━━━━━━━━━━━━━━━━━━━╯\n"
-        string = "• "
-        string3 = "╭━━━━━━━━━━━━━━━━━━━━╮\n   Untuk melihat lengkap Command\n   Contoh: .help < nama module >\n   Modules Aktif: 250\n╰━━━━━━━━━━━━━━━━━━━━╯\n"
-        string2 = " ╾───────────────────────╼"
-        string4 = "\n╰━┉┄═━┉┄═━┉✫┄═━═━┉┄═━╯\
-                          \n **Mod By**➳͜͡❂ঔৣ⃕͜x͠N͜͡ᎬᎳᏴᏆᎬ"
+        await event.edit(f"**╭━━━━━━━━━━━━━━━━━━━━━╮**\
+            \n│   Help for [🔥XBOT-REMIX🔥]\
+            \n╰━━━━━━━━━━━━━━━━━━━━━╯ \
+            \n╭━━━━━━━━━━━━━━━━━━━━━╮\
+            \n│   Untuk melihat lengkap Command\
+            \n│   Contoh: .help <nama module>\
+            \n│   Modules Aktif: {len(modules)}\
+           \n╰━━━━━━━━━━━━━━━━━━━━━╯")
+        string = ""
         for i in CMD_HELP:
             string += "`" + str(i)
-            string += "` •  "
-        await event.edit(
-            f"{string1}" f"{string3}" f"{string2}\n" f"{string}\n" f"{string4}"
-        )
-        await asyncio.sleep(60)
+            string += "`\t•\t "
+        await event.reply(f"{string}"
+                         "\n\n╰━┉┄═━┉┄═━┉✫┄═━═━┉┄═━╯\
+                          \n**Mod By**'➳͜͡❂ঔৣ⃕͜x͠N͜͡ᎬᎳᏴᏆᎬ࿐'")
+        await asyncio.sleep(100)
         await event.delete()
