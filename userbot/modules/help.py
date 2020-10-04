caa40c56c9546d4f56d277d5e699a2829c9d9ea0
X-Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-10-04 08:40:58 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -36,8 +36,8 @@ async def help(event):
         for i in CMD_HELP:
             string += "`" + str(i)
             string += "`\t• "
-        await event.reply(f"{string}"
+        await event.reply(f"*{string}*"
                           "\n╾─────────────────────╼\
-                          \n   **Mod By** :➳͜͡❂ঔৣ⃕͜x͠N͜͡ᎬᎳᏴᏆᎬ࿐")
+                          \n   _MOD BY_ :➳͜͡❂ঔৣ⃕͜x͠N͜͡ᎬᎳᏴᏆᎬ࿐")
         await asyncio.sleep(100)
         await event.delete()
