4ef7f36e2ed7976b28737d7f9d8fae6fdde5b112
X-Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-10-07 16:35:24 +0700

    Pylin jembud

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -79,7 +79,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
         else:
             remote = repo.create_remote("heroku", heroku_git_url)
         try:
-            remote.push(refspec="HEAD:refs/heads/x-sql-extended", force=True)
+            remote.push(refspec="HEAD:refs/heads/x-sql-extended", force=True) #pylint:disable=E0602
         except GitCommandError as error:
             await event.edit(f'{txt}\n`Here is the error log:\n{error}`')
             return repo.__del__()
@@ -163,8 +163,8 @@ async def upstream(event):
         origin = repo.create_remote("upstream", off_repo)
         origin.fetch()
         force_update = True
-        repo.create_head("master", origin.refs.x - sql - extended)
-        repo.heads.master.set_tracking_branch(origin.refs.x - sql - extended)
+        repo.create_head("master", origin.refs.x-sql-extended) #pylint:disable=E0602
+        repo.heads.master.set_tracking_branch(origin.refs.x-sql-extended) #pylint:disable=E0602
         repo.heads.master.checkout(True)
 
     ac_br = repo.active_branch.name
