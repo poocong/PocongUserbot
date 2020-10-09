7681fb5479d28ecc1f580e441f0d28eda4e0edc6
X-Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-10-09 20:44:25 +0700

    Add new modules

---
+++ userbot/modules/phreaker.py
@@ -0,0 +1,87 @@
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from userbot import bot, CMD_HELP
+from userbot.events import register
+
+
+@register(outgoing=True, pattern=r"^\.nmap(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    link = event.pattern_match.group(1)
+    chat = "@scriptkiddies_bot"
+    nmap = f"nmap"
+    await event.edit("Processing....")
+    async with bot.conversation("@scriptkiddies_bot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(
+                    incoming=True,
+                    from_users=510263282))
+            await conv.send_message(f'/{nmap} {link}')
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("Unblock @ scriptkiddies_bot dulu Goblok!!")
+            return
+        else:
+            await event.edit(f"{response.message.message}")
+            await event.client.delete_messages(event.chat.id, conv.chat_id, [msg.id, response.id])
+
+
+@register(outgoing=True, pattern=r"^\.subd(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    link = event.pattern_match.group(1)
+    chat = "@scriptkiddies_bot"
+    subd = f"subdomain"
+    await event.edit("Processing....")
+    async with bot.conversation("@scriptkiddies_bot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(
+                    incoming=True,
+                    from_users=510263282))
+            await conv.send_message(f'/{subdomain} {link}')
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("Unblock @ scriptkiddies_bot dulu Goblok!!")
+            return
+        else:
+            await event.edit(f"{response.message.message}")
+            await event.client.delete_messages(event.chat.id, conv.chat_id, [msg.id, response.id])
+
+
+@register(outgoing=True, pattern=r"^\.cek(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    link = event.pattern_match.group(1)
+    chat = "@scriptkiddies_bot"
+    nmap = f"nmap"
+    await event.edit("Processing....")
+    async with bot.conversation("@scriptkiddies_bot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(
+                    incoming=True,
+                    from_users=510263282))
+            await conv.send_message(f'/{httpheader} {link}')
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("Unblock @ scriptkiddies_bot dulu Goblok!!")
+            return
+        else:
+            await event.edit(f"{response.message.message}")
+            await event.client.delete_messages(event.chat.id, conv.chat_id, [msg.id, response.id])
+
+
+CMD_HELP.update({
+    "phreaker":
+    "`.nmap <bug hosts>`\
+\nUsage: to get info bug/host.\
+\n\n`.subd <bug hosts>`\
+\nUsage: to get subdomain bug/host.\
+\n\n`.cek <bug hosts>`\
+\nUsage: to cek respons bug/host."
+})    
