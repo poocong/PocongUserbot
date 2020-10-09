4dade55b98698cd4c9251a932eb8045a76872cb5
X-Newbie <sxnewbie@gmail.com>
2020-10-09 13:50:30 +0000

    pylint: auto fixes
    Signed-off-by: X-Newbie <sxnewbie@gmail.com>

--- userbot/modules/phreaker.py
+++ userbot/modules/phreaker.py
@@ -9,8 +9,8 @@ async def _(event):
     if event.fwd_from:
         return
     link = event.pattern_match.group(1)
-    chat = "@scriptkiddies_bot" # pylint:disable=E0602
-    nmap = f"nmap" # pylint:disable=E0602
+    chat = "@scriptkiddies_bot"  # pylint:disable=E0602
+    nmap = f"nmap"  # pylint:disable=E0602
     await event.edit("Processing....")
     async with bot.conversation("@scriptkiddies_bot") as conv:
         try:
@@ -33,8 +33,8 @@ async def _(event):
     if event.fwd_from:
         return
     link = event.pattern_match.group(1)
-    chat = "@scriptkiddies_bot" # pylint:disable=E0602
-    subd = f"subdomain" # pylint:disable=E0602
+    chat = "@scriptkiddies_bot"  # pylint:disable=E0602
+    subd = f"subdomain"  # pylint:disable=E0602
     await event.edit("Processing....")
     async with bot.conversation("@scriptkiddies_bot") as conv:
         try:
@@ -57,8 +57,8 @@ async def _(event):
     if event.fwd_from:
         return
     link = event.pattern_match.group(1)
-    chat = "@scriptkiddies_bot" # pylint:disable=E0602
-    httph = f"httpheader" # pylint:disable=E0602
+    chat = "@scriptkiddies_bot"  # pylint:disable=E0602
+    httph = f"httpheader"  # pylint:disable=E0602
     await event.edit("Processing....")
     async with bot.conversation("@scriptkiddies_bot") as conv:
         try:
@@ -84,4 +84,4 @@ CMD_HELP.update({
 \nUsage: to get subdomain bug/host.\
 \n\n`.cek <bug hosts>`\
 \nUsage: to cek respons bug/host."
-})    
+})
