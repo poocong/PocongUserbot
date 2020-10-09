105a3c03ea077ad7f91e1b26dbd4f76cfb7f6b6d
X-Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-10-09 20:05:41 +0700

    Create nmap.py

---
+++ userbot/modules/nmap.py
@@ -0,0 +1,29 @@
+import datetime
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+from userbot import bot
+from userbot.events import register
+ 
+@register(outgoing=True, pattern="^\.nmap(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    link = event.pattern_match.group(1)
+    chat = "@scriptkiddies_bot"
+    nmap = f"nmap"
+    await event.edit("
+Processing
+")
+    async with bot.conversation("@scriptkiddies_bot") as conv:
+          try:
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=510263282))
+              await conv.send_message(f'/{nmap} {link}')
+              response = await response
+          except YouBlockedUserError:
+              await event.reply("
+Unblock @scriptkiddies_bot dulu Goblok!!")
+              return
+          else:
+             await event.edit(f"{response.message.message}")
+             await event.client_delete(chat, conv.id)
