4528f1287ec7c955d73794d70a92ea950565f818
X-Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-10-18 19:05:14 +0700

    Fix wallpapers now with file document

--- userbot/modules/wallpaper.py
+++ userbot/modules/wallpaper.py
@@ -1,44 +1,62 @@
-import os
+# Copyright (C) 2020 Alfiananda P.A
+#
+# Licensed under the General Public License, Version 3.0;
+# you may not use this file except in compliance with the License.
+#
 
-from telethon import events
+import asyncio
+import os
+from asyncio.exceptions import TimeoutError
+
 from telethon.errors.rpcerrorlist import YouBlockedUserError
+
+from userbot import CMD_HELP, bot
 from userbot.events import register
-from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY, bot
 
 
 @register(outgoing=True, pattern=r"^\.wall(?: |$)(.*)")
 async def _(event):
-    if event.fwd_from:
-        return
-    link = event.pattern_match.group(1)
-    wall = f"wall"
-    await event.edit("```Processing```")
-    async with bot.conversation("@XBOTGBOT") as conv:
-        try:
-            response = conv.wait_event(
-                events.NewMessage(
-                    incoming=True,
-                    from_users=1152386221))
-            await conv.send_message(f'/{wall} {link}')
-            response = await response
-        except YouBlockedUserError:
-            await event.edit("```Unblock @XBOTGBOT dulu Goblok!!```")
-            return
-        else:
-            downloaded_file_name = await event.client.download_media(
-                response.media,
-                TEMP_DOWNLOAD_DIRECTORY
-            )
-            await event.client.send_file(
-                event.chat_id,
-                downloaded_file_name,
-                force_document=False,
-            )
-            os.remove(downloaded_file_name)
-            await event.delete()
-            await event.client.delete_messages(event.chat_id,
-                                               [msg.id, downloaded_file_name, response.media])
+    try:
+        query = event.pattern_match.group(1)
+        await event.edit("`Processing..`")
+        async with bot.conversation("@XBOTGBOT") as conv:
+            try:
+                query1 = await conv.send_message(f"/wall {query}")
+                asyncio.sleep(3)
+                r1 = await conv.get_response()
+                r2 = await conv.get_response()
+                await bot.send_read_acknowledge(conv.chat_id)
+            except YouBlockedUserError:
+                return await event.reply("Unblock @XBOTGBOT plox")
+            if r1.text.startswith("No"):
+                return await event.edit(f"`No result found for` **{query}**")
+            else:
+                img = await event.client.download_media(r1)
+                img2 = await event.client.download_media(r2)
+                await event.edit("`Uploading..`")
+                p = await event.client.send_file(
+                    event.chat_id,
+                    img,
+                    force_document=False,
+                    caption="Preview",
+                    reply_to=event.reply_to_msg_id,
+                )
+                await event.client.send_file(
+                    event.chat_id,
+                    img2,
+                    force_document=True,
+                    caption=f"{query}",
+                    reply_to=p,
+                )
+                await event.client.delete_messages(
+                    conv.chat_id, [r1.id, r2.id, query1.id]
+                )
+        await event.delete()
+        os.system("rm *.png *.jpg")
+    except TimeoutError:
+        return await event.edit("`@XBOTGBOT isnt responding..`")
 
 
-CMD_HELP.update({"wallpaper": ">`.wall` <query>."
-                 "\nUsage: Search some wallpaper picture."})
+CMD_HELP.update(
+    {"wallpaper": ">`.wall <query>`" "\nUsage: search beautiful wallpaper image."}
+)
