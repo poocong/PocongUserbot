c7f98ea1c707ab69de216343687868724a5812f0
X-Newbie <sxnewbie@gmail.com>
2020-10-18 12:06:00 +0000

    pylint: auto fixes
    Signed-off-by: X-Newbie <sxnewbie@gmail.com>

--- userbot/modules/wallpaper.py
+++ userbot/modules/wallpaper.py
@@ -57,6 +57,5 @@ async def _(event):
         return await event.edit("`@XBOTGBOT isnt responding..`")
 
 
-CMD_HELP.update(
-    {"wallpaper": ">`.wall <query>`" "\nUsage: search beautiful wallpaper image."}
-)
+CMD_HELP.update({"wallpaper": ">`.wall <query>`"
+                 "\nUsage: search beautiful wallpaper image."})
