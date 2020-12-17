1275e4a7368e819132e576746ea93911a9213d42
X-Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-12-17 08:44:37 +0700

    Fuck

--- userbot/modules/tag_all.py
+++ userbot/modules/tag_all.py
@@ -16,7 +16,7 @@ async def all(event):
     mentions = "@all"
     chat = await event.get_input_chat()
     async for x in bot.iter_participants(chat):
-        mentions += f"[\u2060](tg://user?id={x.id})"
+        mentions += f"[\u2063](tg://user?id={x.id})"
     await bot.send_message(chat, mentions, reply_to=event.message.reply_to_msg_id)
 
 
