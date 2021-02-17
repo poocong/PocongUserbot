f6c0a5f6a0052e711d3984a0ed88b44afd7be2bd
X-ImFine <69149585+X-Newbie@users.noreply.github.com>
2021-02-17 15:40:06 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -124,7 +124,7 @@ BITLY_TOKEN = os.environ.get("BITLY_TOKEN", None)
 BOT_VER = os.environ.get("BOT_VER", "X.1")
 
 # Sticker Custom Pack Name
-S_PACK_NAME = os.environ.get("S_PACK_NAME", "Remix")
+S_PACK_NAME = os.environ.get("S_PACK_NAME", "Remix_Packs")
 
 # Zipfile module
 ZIP_DOWNLOAD_DIRECTORY = os.environ.get("ZIP_DOWNLOAD_DIRECTORY", "./zips")
