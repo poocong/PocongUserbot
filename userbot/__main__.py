6455b4e63389059657e290e2447f9323c35aba27
X-ImFine <69149585+X-Newbie@users.noreply.github.com>
2021-02-17 15:44:36 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -30,7 +30,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"🔥 XBOT-REMIX 🔥 V{BOT_VER} [SUCCESSFUL ACTIVATED! ⚙️]")
+    "🔥 XBOT-REMIX 🔥 V.5 [SUCCESSFUL ACTIVATED! ⚙️]")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
