1b26c2a5a92911ddaf84e4cc8073c02a12676f8a
X-ImFine <69149585+X-Newbie@users.noreply.github.com>
2021-02-20 11:24:36 +0700

    Update tools.py

--- userbot/utils/tools.py
+++ userbot/utils/tools.py
@@ -148,3 +148,20 @@ async def check_media(reply_message):
         return False
     else:
         return data
+
+def convert_toimage(image):
+    img = Image.open(image)
+    if img.mode != "RGB":
+        img = img.convert("RGB")
+    img.save("./temp/temp.jpg", "jpeg")
+    os.remove(image)
+    return "./temp/temp.jpg"
+
+
+async def convert_tosticker(image):
+    img = Image.open(image)
+    if img.mode != "RGB":
+        img = img.convert("RGB")
+    img.save("./temp/temp.webp", "webp")
+    os.remove(image)
+    return "./temp/temp.webp"
