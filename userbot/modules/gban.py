9efdfaa107ae5f786761c43cfcf429851182dbbe
X-ImFine <69149585+X-Newbie@users.noreply.github.com>
2021-02-20 08:29:02 +0700

    Update gban.py

--- userbot/modules/gban.py
+++ userbot/modules/gban.py
@@ -1,9 +1,14 @@
-
-
 from telethon.tl.functions.contacts import BlockRequest, UnblockRequest
-from userbot import CMD_HELP
+import html
+from telethon import events
+from telethon.tl.functions.photos import GetUserPhotosRequest
+from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
+from telethon.utils import get_input_location
+from telethon.events import ChatAction
 
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
+from userbot.utils import admin_cmd
 
 async def get_full_user(event):
     args = event.pattern_match.group(1).split(':', 1)
