be8fca35b8e2e9cabd9099ea24abe963f61bca11
X-ImFine <69149585+X-Newbie@users.noreply.github.com>
2021-02-21 09:23:37 +0700

    Update phreaker.py

--- userbot/modules/phreaker.py
+++ userbot/modules/phreaker.py
@@ -107,28 +107,6 @@ async def _(event):
                 )
 
 
-@register(outgoing=True, pattern=r"^\.cc(?: |$)(.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    link = event.pattern_match.group(1)
-    chat = "@Carol5_bot"  # pylint:disable=E0602
-    ss = f"ss"  # pylint:disable=E0602
-    await event.edit("Processing....")
-    async with bot.conversation("@Carol5_bot") as conv:
-        try:
-            response = conv.wait_event(
-                events.NewMessage(
-                    incoming=True,
-                    from_users=1247032902))
-            await conv.send_message(f'/{ss} {link}')
-            response = await response
-        except YouBlockedUserError:
-            await event.reply("Unblock @Carol5_bot dulu Goblok!!")
-            return
-        else:
-            await event.edit(f"{response.message.message}")
-            await event.client.delete_messages(response.message.message)
 
 
 CMD_HELP.update({
@@ -140,7 +118,5 @@ CMD_HELP.update({
 \n\n`.cek <bug hosts>`\
 \nUsage: to cek respons bug/host.\
     \n\n`.bin < bin number >`\
-    \nUsage: to cek bin ip.\
-\n\n`.cc <mm|yy|cvv`\
-\nUsage: to cek Credits Card Stats."
+    \nUsage: to cek bin ip."
 })
