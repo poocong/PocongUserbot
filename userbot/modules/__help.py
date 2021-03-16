42b9ca4af70271aa9bdf383068aa8e2fcf3119b6
X-ImFine <69149585+X-Newbie@users.noreply.github.com>
2021-03-16 17:11:07 +0700

    Update __help.py

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -1,47 +1,40 @@
 # Copyright (C) 2020 TeamDerUntergang.
 #
-# SedenUserBot is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
 #
-# SedenUserBot is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # @Qulec tarafından yazılmıştır.
 # Thanks @Spechide.
 
 import logging
 
+from telethon.errors.rpcerrorlist import BotInlineDisabledError
 
-from userbot import BOT_USERNAME
+from userbot import BOT_TOKEN, BOT_USERNAME
 from userbot.events import register
 
 logging.basicConfig(
-    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
-    level=logging.WARNING)
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.WARNING
+)
 
 
 @register(outgoing=True, pattern=r"^\.helpme")
 async def yardim(event):
-    try:
-        tgbotusername = BOT_USERNAME
-        if tgbotusername is not None:
-            results = await event.client.inline_query(tgbotusername, "@UserButt")
-            await results[0].click(
-                event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
+    tgbotusername = BOT_USERNAME
+    if tgbotusername and BOT_TOKEN:
+        try:
+            results = await event.client.inline_query(tgbotusername, "@ProjectAlf")
+        except BotInlineDisabledError:
+            return await event.edit(
+                "`Bot can't be used in inline mode.\nMake sure to turn on inline mode!`"
             )
-            await event.delete()
-        else:
-            await event.edit(
-                "`The bot doesn't work! Please set the Bot Token and Username correctly. The module has been stopped.`"
-            )
-    except Exception:
+        await results[0].click(
+            event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
+        )
+        await event.delete()
+    else:
         return await event.edit(
-            "`You cannot send inline results in this chat (caused by SendInlineBotResultRequest)`"
+            "`The bot doesn't work! Please set the Bot Token and Username correctly.`"
+            "\n`The module has been stopped.`"
         )
