b9d4d3c4bf055e9eee42cd351dbf7bb24ca620e5
poocong <81405011+poocong@users.noreply.github.com>
2021-03-27 13:43:58 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -120,7 +120,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "   ⠀  ⠀☆。*。☆。\n     ★。＼｜／。★\n[POCONG USERBOT](https://github.com/poocong/Pocong-Userbot)\n     ★。／｜＼。★\n   ⠀  ⠀☆。*。☆。\n")
+        "   ⠀  ⠀☆。*。☆。\n    ★。＼｜／。★\n[POCONG USERBOT](https://github.com/poocong/Pocong-Userbot)\n    ★。／｜＼。★\n     ⠀☆。*。☆。\n")
 
 
 @register(outgoing=True, pattern="^.raw$")
