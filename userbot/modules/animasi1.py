9aa0a27da5e6e8642e12767f068111636663de0a
poocong <81405011+poocong@users.noreply.github.com>
2021-04-06 07:12:28 +0700

    Update animasi1.py

--- userbot/modules/animasi1.py
+++ userbot/modules/animasi1.py
@@ -8,44 +8,45 @@ import asyncio
 @register(outgoing=True, pattern='^.hai(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(3)
+    sleep(1)
     await typew.edit("**Hai ,  Assalamualaikum**")
-    sleep(2)
+    sleep(1)
     await typew.edit("Kalian Nungguin aku gak??")
-    sleep(2)
+    sleep(1)
     await typew.edit("Ih ga mau🤢")
-    sleep(2)
+    sleep(1)
     await typew.edit("gasukaa😫")
-    sleep(2)
+    sleep(1)
     await typew.edit("__GELAYY__🤮")
     
    
 @register(outgoing=True, pattern='^.kntl(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(3)
+    sleep(1)
     await typew.edit("Tau kh kalian wahai tuan-tuan??")
-    sleep(2)
+    sleep(1)
     await typew.edit("se**KONT0L** **K0NTOL** nya si **K0NTOL**")
-    sleep(2)
+    sleep(1)
     await typew.edit("lebih **KONTOL** lagi")              
-    sleep(2)
+    sleep(1)
     await typew.edit("kalian **KONTOL**")
 
 
 @register(outgoing=True, pattern='^.alay(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(3)
+    sleep(1)
     await typew.edit("eh kamu, iya kamu")
-    sleep(2)
+    sleep(1)
     await typew.edit("**ALAY** bnget sih")
-    sleep(2)
+    sleep(1)
     await typew.edit("spam bot mulu")
-    sleep(2)
+    sleep(1)
     await typew.edit("baru jadi userbot ya?? xixixi")
-    sleep(2)
+    sleep(1)
     await typew.edit("pantes **NORAK**")
+
 
 CMD_HELP.update({
     "animasi1":
