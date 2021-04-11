7688d2d46d5569537e7b3a2c63e3b6967fc86ca5
poocong <81405011+poocong@users.noreply.github.com>
2021-04-12 06:45:15 +0700

    farhan kintil

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -20,7 +20,7 @@ async def help(event):
         if args in CMD_HELP:
             await event.edit(str(CMD_HELP[args]))
         else:
-            await event.edit("**Maaf Tuan, Saya Tidak Punya Perintah Itu ツ**")
+            await event.edit("**Module Yg Lu Cari Gada Gblk**")
             await asyncio.sleep(200)
             await event.delete()
     else:
