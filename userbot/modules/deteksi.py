fe90c709458a31a0da25a871a0cfc8a0c8b4ba97
poocong <81405011+poocong@users.noreply.github.com>
2021-04-13 10:54:50 +0700

    cie kepo

--- userbot/modules/deteksi.py
+++ userbot/modules/deteksi.py
@@ -1,6 +1,6 @@
-# Ported From Cat Userbot For Lord Userbot By Alvin/LiuAlvinas # Jangan Hapus # Jangan Ubah
+# Ported From Cat Userbot For Ocong Userbot By Pocong Onlen # Jangan Hapus # Jangan Ubah
 # Based On Plugins
-# Alvin Ganteng
+# Fakeghost
 
 
 from telethon.errors.rpcerrorlist import YouBlockedUserError
