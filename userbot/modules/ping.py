a5aad49db46f78a9b98eab4e09cca695f44459e3
poocong <81405011+poocong@users.noreply.github.com>
2021-04-15 20:55:25 +0700

    R.I.P English

--- userbot/modules/ping.py
+++ userbot/modules/ping.py
@@ -80,7 +80,7 @@ async def _(event):
               f"`•Ping: {ping_time}\n`"
               f"`•ISP: {i_s_p}\n`"
               f"`•ISP Rating: {i_s_p_rating}\n\n`"
-              "**POWERED BY XBOT REMIX 🔥**")
+              "**POWERED BY POCONG USERBOT 🔥**")
     await bot.send_file(
         event.chat_id,
         speedtest_image,
@@ -99,7 +99,7 @@ async def pingme(pong):
     await pong.edit("`Pinging....`")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
-    await pong.edit(f"**PONG!! 🍭**\n**Pinger** : %sms\n**Bot Uptime** : {uptime}🕛" % (duration))
+    await pong.edit(f"☤ **𓆩Pong𓆪**\n**➦Ping __%sms__\n****➥Bot Uptime** : {uptime}🕛" % (duration))
 
 
 @register(outgoing=True, pattern="^.pong$")
