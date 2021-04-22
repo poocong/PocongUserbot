8603f2f4aa160ae706cb9fc53d4b0c033cc3f068
poocong <81405011+poocong@users.noreply.github.com>
2021-04-22 22:06:18 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -223,12 +223,12 @@ async def amireallyalive(alive):
     user = await bot.get_me()
     await get_readable_time((time.time() - StartTime))
     output = (
-        f"**𝙋𝙤𝙘𝙤𝙣𝙜𝙐𝙨𝙚𝙧𝙗𝙤𝙩  //  𝙓-𝘽𝙤𝙩𝙍𝙚𝙢𝙞𝙭** \n"
-        f"😈 Master: `{DEFAULTUSER}` \n"
-        f"⚙️ Telethon: `Versi {version.__version__}` \n"
-        f"🐍 Python:  `Versi {python_version()}` \n"
-        f"🤖 Bot Versi: `{BOT_VER}` \n"
-        f"🗃 Plugins: `{len(modules)} Plugins` \n\n"
+        f"**𝙋𝙤𝙘𝙤𝙣𝙜𝙐𝙨𝙚𝙧𝙗𝙤𝙩  𝙏𝙚𝙡𝙖𝙝𝘼𝙠𝙩𝙞𝙛** \n"
+        f"┣[❏😈 Master: `{DEFAULTUSER}` \n"
+        f"┣[❏⚙️ Telethon: `Versi {version.__version__}` \n"
+        f"┣[❏🐍 Python:  `Versi {python_version()}` \n"
+        f"┣[❏🤖 Bot Versi: `{BOT_VER}` \n"
+        f"┣[❏🗃 Plugins: `{len(modules)} Plugins` \n\n"
         f"📌 **REPO** [POCONG USERBOT](https://github.com/poocong/Pocong-Userbot)\n")
     if ALIVE_LOGO:
         try:
