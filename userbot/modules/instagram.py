1e98d1399535b8417abf6e8e0218dac79884ffe3
poocong <81405011+poocong@users.noreply.github.com>
2021-04-24 16:46:46 +0700

    Update instagram.py

--- userbot/modules/instagram.py
+++ userbot/modules/instagram.py
@@ -1,8 +1,8 @@
-# Koala Ganteng, Kode Dari Koala Bangsul Press F untuk Koala @Manusiarakitann
-# Keredit Motor Eh Maksudnya Kredit Kampang Bot (c) Koala Bgke @ManusiaRakitann
-# Karna Aku Gabut Aku Pasang Keredit Lagi # Keredit
-# Yak Pasang Credit Banyak Banyak Biar Makin Keren
-# Copyright (C) 2021 Apis / @PacarFerdilla By King Userbot
+# Bismillah
+# Apa Lu Ga Terima?
+# Suka Suka Gw Dong
+# Kok Ngatur
+# Pap TT kak 👻
 # All rights reserved.
 # Keredit
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
@@ -26,11 +26,11 @@ async def insta(event):
     if event.fwd_from:
         return
     if not event.reply_to_msg_id:
-        await event.edit("`King, Mohon Balas Ke Link Instagram`")
+        await event.edit("`Mohon Balas Ke Link Instagram`")
         return
     reply_message = await event.get_reply_message()
     if not reply_message.text:
-        await event.edit("`Mohon Maaf King, Saya Membutuhkan Link Media Instagram Untuk Download`")
+        await event.edit("`Mohon Maaf, Saya Membutuhkan Link Media Instagram Untuk Download`")
         return
     chat = "@SaveAsBot"
     reply_message.sender
@@ -46,7 +46,7 @@ async def insta(event):
             await event.client.send_message(chat, reply_message)
             response = await response
         except YouBlockedUserError:
-            await event.edit("`King, Mohon Buka Blokir` @SaveAsbot `Lalu Coba Lagi`")
+            await event.edit("`Mohon Buka Blokir` @SaveAsbot `Lalu Coba Lagi`")
             return
         if response.text.startswith("Forward"):
             await event.edit(
@@ -57,15 +57,15 @@ async def insta(event):
             await event.client.send_file(
                 event.chat_id,
                 response.message.media,
-                caption=f"**Download By @USEEBOT_GROUP**",
+                caption=f"**Creator @Pocongonlen**",
             )
             await event.client.send_read_acknowledge(conv.chat_id)
             await bot(functions.messages.DeleteHistoryRequest(peer=chat, max_id=0))
             await event.delete()
 
 
-# By King - Userbot
-# Apis Gansssssss Mksh Sma Sma
-# Apis Gans
-CMD_HELP.update({"instagram": "**Modules:** __Instagram__\n\n⚡𝘾𝙈𝘿⚡: `.ig`"
+# By Pocong - Userbot
+# Hmm
+# Dah lah Cape 
+CMD_HELP.update({"instagram": "**Modules:** __Instagram__\n\n𝘾𝙈𝘿: `.ig`"
                  "\n**Penjelasan:** Download Media di Postingan Instagram, Balas ke link instagram ketik `.ig`"})
