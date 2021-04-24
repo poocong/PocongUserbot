b82e8d2ffd1c271c1872f16721e8cd65fa549eb2
poocong <81405011+poocong@users.noreply.github.com>
2021-04-24 21:34:43 +0700

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -80,7 +80,7 @@ async def _(event):
               f"`•Ping: {ping_time}\n`"
               f"`•ISP: {i_s_p}\n`"
               f"`•ISP Rating: {i_s_p_rating}\n\n`"
-              "**POWERED BY XBOT REMIX 🔥**")
+              "**POWERED BY POCONG USERBOT 🔥**")
     await bot.send_file(
         event.chat_id,
         speedtest_image,
