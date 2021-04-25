63abe7c5d374c82fdb45c72040192e56271d0dae
poocong <81405011+poocong@users.noreply.github.com>
2021-04-25 19:50:03 +0700

    pylint: auto fixes

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -124,8 +124,8 @@ async def pingme(pong):
 
     
 @register(outgoing=True, pattern="^.dc$")
-async def neardc(event)
-""" For .dc command, get the nearest datacenter information. """
+async def neardc(event):
+    """ For .dc command, get the nearest datacenter information. """
     result = await event.client(functions.help.GetNearestDcRequest())
     await event.edit(f"Country : `{result.country}`\n"
                      f"Nearest Datacenter : `{result.nearest_dc}`\n"
