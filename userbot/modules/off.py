e99f6b5b1b4d2ec22e4049c1755c963429e9ceb9
poocong <81405011+poocong@users.noreply.github.com>
2021-04-28 16:59:31 +0700

    Update off.py

--- userbot/modules/off.py
+++ userbot/modules/off.py
@@ -286,10 +286,10 @@ async def type_afk_is_not_true(notafk):
 
 CMD_HELP.update(
     {
-        "off":"𝐒𝐲𝐧𝐭𝐚𝐱:`.off [Alasan Opsional]`"
+        "off":"𝐒𝐲𝐧𝐭𝐚𝐱:` .off`"
         "\n𝐅𝐮𝐧𝐜𝐭𝐢𝐨𝐧: Menetapkan Anda sebagai afk. Balas kepada siapa saja yang memberi tag/PM's "
         "Anda, memberi tahu mereka bahwa Anda AFK(karena)."
-        "\n\𝐒𝐲𝐧𝐭𝐚𝐱:`.unoff`"
+        "\n\n𝐒𝐲𝐧𝐭𝐚𝐱:` .unoff`"
         "\n𝐅𝐮𝐧𝐜𝐭𝐢𝐨𝐧: Mematikan mode AFK anda"
     }
 )
