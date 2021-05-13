ffbb8ae5669cd5bed8958fe2037c93345011b387
poocong <81405011+poocong@users.noreply.github.com>
2021-05-13 09:49:20 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -392,7 +392,10 @@ with bot:
         @tgbot.on(events.CallbackQuery(data=b'close'))
         async def close(event):
             await event.edit("Button closed!", buttons=Button.clear())
-
+        else:
+            reply_pop_up_alert = f"Harap Deploy 𝙿𝚘𝚌𝚘𝚗𝚐 𝚄𝚜𝚎𝚛𝚋𝚘𝚝 Anda Sendiri, Jangan Menggunakan Milik {ALIVE_NAME}"
+            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+            
         @tgbot.on(
             events.callbackquery.CallbackQuery(  # pylint:disable=E0602
                 data=re.compile(rb"helpme_prev\((.+?)\)")
