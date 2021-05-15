f01a54066a35582c1b8c78ed6789425be777c358
poocong <81405011+poocong@users.noreply.github.com>
2021-05-15 08:08:46 +0700

    Update animasi2.py

--- userbot/modules/animasi2.py
+++ userbot/modules/animasi2.py
@@ -303,29 +303,29 @@ async def typewriter(typew):
 
 CMD_HELP.update({
     "animasi2":
-    "•**Syntax**: .bulan\
-    \n   •**Function**: Cek Aja dh\
-    \n\n•**Syntax**: .hati\
-    \n   •**Function**: liat aja\
-    \n\n•**Syntax**: .bernyanyi\
-    \n   •**Function**: Cek Aja dh\
-    \n\n•**Syntax**: .helikopter\
-    \n   •**Function**: Liat Aja dah Sendri \
-    \n\n•**Syntax**: .bundir\
-    \n   •**Function**: Lelah Adek Bang\
-    \n\n•**Syntax**: .y\
-    \n   •**Function**: jempol\
-    \n\n•**Syntax**: .hahaha\
-    \n   •**Function**: ketawa lari\
-    \n\n•**Syntax**: .babi\
-    \n   •**Function**: Babi Ngook\
-    \n\n•**Syntax**: .ajg\
-    \n   •**Function**: Anjing mirip kelakuan lu\
-    \n\n•**Syntax**: .ular\
-    \n   •**Function**: Ular kadut\
-    \n\n•**Syntax**: .tembak\
-    \n   •**Function**: Nembak Cewe\
-    \n\n•**Syntax**: .tank\
-    \n   •**Function**: Tanktop"
+    "•   **Syntax**: .bulan\
+    \n•**Function**: Cek Aja dh\
+    \n\n   •**Syntax**: .hati\
+    \n•**Function**: liat aja\
+    \n\n   •**Syntax**: .bernyanyi\
+    \n•**Function**: Cek Aja dh\
+    \n\n   •**Syntax**: .helikopter\
+    \n•**Function**: Liat Aja dah Sendri \
+    \n\n   •**Syntax**: .bundir\
+    \n•**Function**: Lelah Adek Bang\
+    \n\n   •**Syntax**: .y\
+    \n•**Function**: jempol\
+    \n\n   •**Syntax**: .hahaha\
+    \n•**Function**: ketawa lari\
+    \n\n   •**Syntax**: .babi\
+    \n•**Function**: Babi Ngook\
+    \n\n   •**Syntax**: .ajg\
+    \n•**Function**: Anjing mirip kelakuan lu\
+    \n\n   •**Syntax**: .ular\
+    \n•**Function**: Ular kadut\
+    \n\n   •**Syntax**: .tembak\
+    \n•**Function**: Nembak Cewe\
+    \n\n   •**Syntax**: .tank\
+    \n•**Function**: Tanktop"
 })
 
