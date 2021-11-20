2dd81437e3e896bb0e222f3571ee8ed1a83d8139
poocong <81405011+poocong@users.noreply.github.com>
2021-11-21 01:21:51 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -27,9 +27,9 @@ async def help(event):
         string = ""
         for i in CMD_HELP:
             string += "`" + str(i)
-            string += "`\t📂"
+            string += "`\t❐"
         await event.edit("**𓇕 Berikut Daftar Perintah Untuk\nPocong-Userbot:\n\n**"
-                         f"📂 {string} 📂\n")
+                         f"❐ {string}\n")
         await event.reply(f"\n**Ketik Contoh** `.help afk` **Untuk Informasi Perintah**")
         await asyncio.sleep(1000)
         await event.delete()
