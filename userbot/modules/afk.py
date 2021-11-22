c087894d98d15906ff38de679f569bf313e900b9
poocong <81405011+poocong@users.noreply.github.com>
2021-11-22 20:51:01 +0700

    Update afk.py

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -112,8 +112,8 @@ async def on_afk(event):
             message_to_reply = (
                   f"┌ ❏AFK!\n"
                 + f"│┌ **User** {ALIVE_NAME}\n"
-                + f"│├ **Sejak** {total_afk_time} Yg Lalu\n"
-                + f"└└ **Karena** {reason}"
+                + f"│├ **Karena** {reason}"
+                + f"└└ **AFK!** Sejak {total_afk_time} Yng lalu\n"
                 
             )
         else:
