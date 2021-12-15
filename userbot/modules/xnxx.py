98751174b4a481e70baf921137d30700e09807b4
poocong <81405011+poocong@users.noreply.github.com>
2021-12-15 19:06:40 +0700

    Add files via upload

---
+++ userbot/modules/xnxx.py
@@ -0,0 +1,26 @@
+from time import sleep
+from userbot import CMD_HELP, bot
+from userbot.events import register
+from telethon import events
+import asyncio
+
+
+@register(outgoing=True, pattern='^.dana?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("**PAYMENT ?\n\n Dana & Gopay\n\n ❏085372922123 **")
+    
+
+@register(outgoing=True, pattern='^.via(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("**Untuk Metode Pembayaran.\n\n ❏Dana\n ❏Gopay\n  ❏Pulsa ? + 5k**")
+    
+   
+@register(outgoing=True, pattern='^.thnks(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("**Done yaa kak🙏🏻\n TerimaKasih...\n\n [PocongOnlenStore?](https://t.me/Poocongonlen)\n [Testi](https://t.me/TestimoniPocong)")
